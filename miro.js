7473d3ab3b306c5f22871d45f651bc1192da7639
suxxzzy <suxxzzy@github.com>
2023-03-18 11:42:55 +0900

    80%까지만 맞춤

--- miro.js
+++ miro.js
@@ -11,15 +11,15 @@ function getShortestDist(m, n, startX, startY, ball) {
   //두 공이 x좌표가 같은 경우
   if (startX === ball[0]) {
     const height = Math.min(startX, m - startX); //삼각형 높이
-    const width = Math.abs(startY - ball[1]);
-    return calcDist(height, width);
+    const width = Math.abs(startY - ball[1]) / 2;
+    return Math.round(calcDist(height, width));
   }
 
   //두 공의 y좌표, n - y좌표 중 최소값 구한다
   if (startY === ball[1]) {
     const height = Math.min(startY, n - startY); //삼각형 높이
-    const width = Math.abs(startX - ball[0]);
-    return calcDist(height, width);
+    const width = Math.abs(startX - ball[0]) / 2;
+    return Math.round(calcDist(height, width));
   }
 
   //두 공의 좌표가 모두 다른 경우
@@ -29,7 +29,7 @@ function getShortestDist(m, n, startX, startY, ball) {
     calcDist2(Math.abs(startX - ball[0]), n - startY, n - ball[1]), //북
     calcDist2(Math.abs(startY - ball[1]), startX, ball[0]), //서
   ];
-  return Math.min(...candidate);
+  return Math.round(Math.min(...candidate));
 }
 
 function calcDist(height, width) {
@@ -48,9 +48,3 @@ function calcDist2(widthDist, theight1, theight2) {
   const t2 = calcDistForSIngleTriangle(h2, theight2);
   return Math.pow(t1 + t2, 2);
 }
-
-solution(10, 10, 3, 7, [
-  [7, 7],
-  [2, 7],
-  [7, 3],
-]);
