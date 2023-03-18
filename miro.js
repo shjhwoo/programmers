a8fc4245051c2b66e1a57a7061b3a5e71fafa720
suxxzzy <suxxzzy@github.com>
2023-03-18 11:04:34 +0900

    당구: 최소 이동거리: 시간 내 못끝냄

--- miro.js
+++ miro.js
@@ -1,71 +1,65 @@
-//최소한의 이동거리를 구하려면 가장 경계점에 있는 파일이 무엇인지를 알아야 한다
-//가장 위 - 가장 왼쪽에 있는 파일 하나.
-//가장 아래- 가장 오른쪽에 있는 파일 하나.
-
-//특이 케이스: 파일이 하나뿐인 경우: 바로 처리할 수 있다
-
-function solution(wallpaper) {
-  //파일이 단 하나뿐인 경우
-  if (wallpaper.join("").match(/#/g)?.length === 1) {
-    //파일의 XY 좌표 구하기
-    const fileY = wallpaper.findIndex((line) => line.includes("#"));
-    const fileX = wallpaper[fileY].split("").findIndex((ch) => ch === "#");
-
-    return [fileY, fileX, fileY + 1, fileX + 1];
-  }
-  //가장 위에파일 찾기
-  const fileLY = wallpaper.findIndex((line) => line.includes("#"));
-  //가장 왼쪽에 파일 찾기
-  const fileLX = Math.min(
-    ...wallpaper
-      .map((line) => line.split("").findIndex((ch) => ch === "#"))
-      .filter((n) => n >= 0)
-  );
-
-  //왼쪽 위 교점 구하기
-
-  //가장 아래 파일 찾기
-  const fileRY =
-    wallpaper.length -
-    wallpaper.reverse().findIndex((line) => line.includes("#"));
-
-  //가장 오른쪽 파일 찾기
-  const fileRX = Math.max(
-    ...wallpaper
-      .filter((line) => line.includes("#"))
-      .map(
-        (line) =>
-          line.length -
-          line
-            .split("")
-            .reverse()
-            .findIndex((ch) => ch === "#")
-      )
-      .filter((n) => n >= 0)
-  );
-  return [fileLY, fileLX, fileRY, fileRX + 1];
+function solution(m, n, startX, startY, balls) {
+  var answer = [];
+  balls.forEach((ball) => {
+    const dist = getShortestDist(m, n, startX, startY, ball);
+    answer.push(dist);
+  });
+  return answer;
 }
 
-solution([
-  ".##...##.",
-  "#..#.#..#",
-  "#...#...#",
-  ".#.....#.",
-  "..#...#..",
-  "...#.#...",
-  "....#....",
-]);
-//y좌표, x좌표 순으로 리턴
-//파일 위치가 x: 0 y: 1인 경우
+function getShortestDist(m, n, startX, startY, ball) {
+  //두 공이 x좌표가 같은 경우
+  if (startX === ball[0]) {
+    const height = Math.min(startX, m - startX); //삼각형 높이
+    const width = Math.abs(startY - ball[1]);
+    return calcDist(height, width);
+  }
 
-//왼쪽 위: x: 0 y: 1
-//오른쪽 위: x: 1 y: 1
+  //두 공의 y좌표, n - y좌표 중 최소값 구한다
+  if (startY === ball[1]) {
+    const height = Math.min(startY, n - startY); //삼각형 높이
+    const width = Math.abs(startX - ball[0]);
+    return calcDist(height, width);
+  }
 
-//왼쪽 아래: x: 0 y: 2
-//오른쪽 아래: x: 1 y: 2
+  //두 공의 좌표가 모두 다른 경우
+  const candidate = [];
+  //4군데 다 생각해야 함
+  //남쪽으로 치는 경우
+  //동쪽으로 치는 경우
+  //북쪽으로 치는 경우
+  //서쪽으로 친느 경우
 
-//파일 위치가 x1 y1인 경우
-//왼쪽 위: x1 y1
-//오른쪽 위: x2 y1
-//왼쪽 아래: x1 y2
-//오른쪽 아래: x2 y2
+  //하나만 생각하면
+  //탄젠트 값 구한다
+  //한쪽 삼각형의 높이를 구한다
+  //그 높이와 밑변의 길이 통해서 빗변의 길이를 구한다.
+  const southXDist = Math.abs(startX - ball[0]);
+  const tan1 = southXDist / (startY + ball[1]);
+  const h1 = tan1 * startY;
+  const t1 = calcDistForSIngleTriangle(h1, startY);
+  const h2 = tan1 * ball[1];
+  const t2 = calcDistForSIngleTriangle(h2, ball[1]);
+  Math.pow(t1 + t2, 2);
+
+  //동쪽
+  const eastYDist = Math.abs(startY - ball[1]);
+  const tan2 = eastYDist / (m - startX + m - ball[1]);
+}
+
+function calcDist(height, width) {
+  return Math.pow(Math.sqrt(height * height + width * width) * 2, 2);
+}
+
+function calcDistForSIngleTriangle(height, width) {
+  return Math.sqrt(height * height + width * width);
+}
+
+//문제 분석
+
+//벽에 한번은 맞아야 함
+//입사각 반사각 동일하다
+
+//공1 출발
+
+//공2 도착
