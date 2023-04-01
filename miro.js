b4ad74938433cf8f72e825d34f181a89d90e4e91
suxxzzy <suxxzzy@github.com>
2023-04-01 10:48:19 +0900

    당구문제 풀이 수정: 100%, 통과

--- miro.js
+++ miro.js
@@ -9,43 +9,39 @@ function solution(m, n, startX, startY, balls) {
 
 function getShortestDist(m, n, startX, startY, ball) {
   //두 공이 x좌표가 같은 경우
-  if (startX === ball[0]) {
-    const candidate = [
-      Math.pow(
-        startX > ball[0] ? n - startY + (n - ball[1]) : startY + ball[1],
-        2
-      ),
-      calcDist2(Math.abs(2 * m - startX - ball[0]), Math.abs(startY - ball[1])), //동
-      calcDist2(Math.abs(startX + ball[0]), Math.abs(startY - ball[1])), //서
-    ];
-    return Math.min(...candidate);
+  const [targetX, targetY] = ball;
+
+  const candidate_up =
+    (startX - targetX) ** 2 + (startY - 2 * n + targetY) ** 2;
+  const candidate_right =
+    (startX - 2 * m + targetX) ** 2 + (startY - targetY) ** 2;
+
+  const candidate_down = (startX - targetX) ** 2 + (startY + targetY) ** 2;
+
+  const candidate_left = (startX + targetX) ** 2 + (startY - targetY) ** 2;
+
+  if (startX === targetX) {
+    if (startY < targetY) {
+      return Math.min(candidate_right, candidate_down, candidate_left);
+    } else {
+      return Math.min(candidate_right, candidate_up, candidate_left);
+    }
   }
 
-  //두 공의 y좌표 같은 경우, n - y좌표 중 최소값 구한다
-  if (startY === ball[1]) {
-    const candidate = [
-      Math.pow(
-        startX > ball[0] ? m - startX + (m - ball[0]) : startX + ball[0],
-        2
-      ),
-      calcDist2(Math.abs(startX - ball[0]), Math.abs(startY - ball[1] * -1)), //남
-      calcDist2(Math.abs(startX - ball[0]), Math.abs(2 * n - startY - ball[1])), //북
-    ];
-    return Math.min(...candidate);
+  if (startY === targetY) {
+    if (startX < targetX) {
+      return Math.min(candidate_up, candidate_down, candidate_left);
+    } else {
+      return Math.min(candidate_up, candidate_down, candidate_right);
+    }
   }
 
-  //두 공의 좌표가 모두 다른 경우
-  const candidate = [
-    calcDist2(Math.abs(startX - ball[0]), Math.abs(startY - ball[1] * -1)), //남
-    calcDist2(Math.abs(2 * m - startX - ball[0]), Math.abs(startY - ball[1])), //동
-    calcDist2(Math.abs(startX - ball[0]), Math.abs(2 * n - startY - ball[1])), //북
-    calcDist2(Math.abs(startX + ball[0]), Math.abs(startY - ball[1])), //서
-  ];
-  return Math.min(...candidate);
-}
-
-function calcDist2(w, h) {
-  return Math.pow(w, 2) + Math.pow(h, 2);
+  return Math.min(
+    candidate_down,
+    candidate_left,
+    candidate_right,
+    candidate_up
+  );
 }
 
 solution(10, 10, 3, 7, [
