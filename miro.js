a6014633f9d824a69adca437a178fbaeb9d7e514
shjhwoo <shjhwoo@github.com>
2023-04-08 11:53:14 +0900

    달리기 경주: 이렇게 했는데도 시간 초과함

--- miro.js
+++ miro.js
@@ -1,51 +1,27 @@
-function solution(m, n, startX, startY, balls) {
-  var answer = [];
-  balls.forEach((ball) => {
-    const dist = getShortestDist(m, n, startX, startY, ball);
-    answer.push(dist);
+function solution(players, callings) {
+  let result = Object.fromEntries(players.map((player, idx) => [player, idx]));
+  let res2 = Object.fromEntries(players.map((player, idx) => [idx, player]));
+
+  callings.forEach((player, i) => {
+    const idx = result[player];
+    const prevIdx = idx - 1;
+
+    const prevRes2 = { ...res2 };
+    res2[prevIdx] = player;
+    res2[idx] = prevRes2[prevIdx];
+
+    const prevResult = { ...result };
+    result[player] = prevResult[player] - 1;
+    result[res2[idx]] = prevResult[res2[idx]] + 1;
+
+    console.log(result, res2, i);
   });
-  return answer;
-}
-//이거 점대칭으로 풀어야함.. 그냥 삼각비 쓰면 정수값 안나와서 다 틀림ㅠㅠ
-function getShortestDist(m, n, startX, startY, ball) {
-  //두 공이 x좌표가 같은 경우
-  const [targetX, targetY] = ball;
 
-  const candidate_up =
-    (startX - targetX) ** 2 + (startY - 2 * n + targetY) ** 2;
-  const candidate_right =
-    (startX - 2 * m + targetX) ** 2 + (startY - targetY) ** 2;
-
-  const candidate_down = (startX - targetX) ** 2 + (startY + targetY) ** 2;
-
-  const candidate_left = (startX + targetX) ** 2 + (startY - targetY) ** 2;
-
-  if (startX === targetX) {
-    if (startY < targetY) {
-      return Math.min(candidate_right, candidate_down, candidate_left);
-    } else {
-      return Math.min(candidate_right, candidate_up, candidate_left);
-    }
-  }
-
-  if (startY === targetY) {
-    if (startX < targetX) {
-      return Math.min(candidate_up, candidate_down, candidate_left);
-    } else {
-      return Math.min(candidate_up, candidate_down, candidate_right);
-    }
-  }
-
-  return Math.min(
-    candidate_down,
-    candidate_left,
-    candidate_right,
-    candidate_up
-  );
+  return Object.values(res2);
 }
 
-solution(10, 10, 3, 7, [
-  [7, 7],
-  [2, 7],
-  [7, 3],
-]);
+const res = solution(
+  ["mumu", "soe", "poe", "kai", "mine"],
+  ["kai", "kai", "mine", "mine"]
+);
+console.log(res);
