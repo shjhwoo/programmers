fcaf26b3294c7965c21a5d1af448fd2f128ca25e
shjhwoo <shjhwoo@github.com>
2023-04-15 12:01:17 +0900

    과제 진행하기-진행중

--- solution.js
+++ solution.js
@@ -1,17 +1,70 @@
-function solution(name, yearning, photo) {
-  //name과 yearning을 맵으로 만들어준다. 이름 => 점수
-  const scoreMap = new Map();
-  name.forEach((n, idx) => {
-    scoreMap.set(n, yearning[idx]);
+function solution(plans) {
+  const answer = [];
+
+  //answer <== 진행 중 과제 잠시멈춘과제(스택)
+
+  //제일 먼저 plans 들을 시작시간 순서로 오름차순 정렬해야 한다
+  const formattedPlans = formatPlans(plans);
+  //잠시 멈춘 과제 배열을 선언한다
+  const tempStopped = new Map(); //{name, remain, idx} 멈춘 과제 이름과 남은 시간, 인덱스를 배열로 저장한다
+
+  //정렬된 과제 배열을 순회한다.
+  formattedPlans.forEach((plan, idx) => {
+    //지금 시작한 과제
+    const [name, startTime, requiredTime] = plan;
+    //지금 시작한 과제가 끝나는 시각을 구한다
+    const endTime = new Date(startTime.getTime() + requiredTime * 60000);
+
+    if (formattedPlans[idx + 1] !== undefined) {
+      //다음에 시작해야 하는 과제
+      const [nextName, nextStartTime, nextRequiredTime] =
+        formattedPlans[idx + 1];
+
+      if (endTime > nextStartTime) {
+        if (!tempStopped.has(name)) {
+          tempStopped.set(name, {
+            idx,
+            remain: requiredTime - getMinDiff(nextStartTime - startTime),
+          });
+        } else {
+          //이미 배열에 있는 경우, 시간 다시 계산해준다
+          const idx = tempStopped.get(name).idx;
+          const remain = tempStopped.get(name).remain;
+          tempStopped.set(name, {
+            idx,
+            remain: remain - getMinDiff(nextStartTime - startTime),
+          });
+        }
+      }
+      //제시간에 끝낸경우
+      if (endTime < nextStartTime) {
+        answer.push(name);
+        //잠시 멈춰둔 과제가 있다면 이어서 진행
+        if (tempStopped.size > 0) {
+          const recentStoppped = Array.from(tempStopped).pop();
+        }
+      }
+
+      if (endTime === nextStartTime) {
+        answer.push(name);
+        //잠시 멈춰둔 과제가 있다면 새로 시작하는 과제부터 진행
+        if (tempStopped.size > 0) {
+        }
+      }
+    }
   });
-  //photo 순회하면서 점수 합산한다
-  return photo
-    .map((p) => p.filter((n) => scoreMap.get(n)))
-    .map((p) => getYearningScore(p, scoreMap));
+
+  return answer;
 }
 
-function getYearningScore(photo, scoreMap) {
-  return photo.reduce((sum, n) => {
-    return scoreMap.get(n) !== undefined ? scoreMap.get(n) + sum : sum;
-  }, 0);
+function formatPlans(plans) {
+  return plans.map((plan) => {
+    const startTime = plan[1];
+    const [hh, mm] = startTime.split(":");
+    return [plan[0], new Date(null, null, null, hh, mm), plan[2]];
+  });
+}
+
+function getMinDiff(nextDate, date) {
+  return (((nextDate - date) % 86400000) % 3600000) / 60000;
 }
