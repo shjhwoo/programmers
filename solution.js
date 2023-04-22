d92d2369119b41ad3494e7e3e7a4b1df001e1b42
shjhwoo <shjhwoo@trustnhope.com>
2023-04-22 12:08:03 +0900

    과제 진행하기 2차 ㅅ도

--- solution.js
+++ solution.js
@@ -1,71 +1,117 @@
 function solution(plans) {
-  const answer = [];
+  //시작해야 되는 과제
+  /*
+  멈춰둔 과제 저장소: 스택
+  앞으로 해야 하는 과제 저장소: 큐?
+  */
 
-  //answer <== 진행 중 과제 잠시멈춘과제(스택)
+  const tempStoppedHomeWork = [];
 
-  //제일 먼저 plans 들을 시작시간 순서로 오름차순 정렬해야 한다
-  const formattedPlans = formatPlans(plans);
-  //잠시 멈춘 과제 배열을 선언한다
-  const tempStopped = new Map(); //{name, remain, idx} 멈춘 과제 이름과 남은 시간, 인덱스를 배열로 저장한다
+  //과제 시작 시간 기준 오름차순으로 정렬한다
+  const notStartedHomeworks = formatPlans(plans).sort(
+    (a, b) => a.startTime - b.startTime
+  );
 
-  //정렬된 과제 배열을 순회한다.
-  formattedPlans.forEach((plan, idx) => {
-    //지금 시작한 과제
-    const [name, startTime, requiredTime] = plan;
-    //지금 시작한 과제가 끝나는 시각을 구한다
-    const endTime = new Date(startTime.getTime() + requiredTime * 60000);
+  //현재 진행중인 과제
+  const onFocus = [];
 
-    if (formattedPlans[idx + 1] !== undefined) {
-      //다음에 시작해야 하는 과제
-      const [nextName, nextStartTime, nextRequiredTime] =
-        formattedPlans[idx + 1];
+  //완료한 과제가 쌓이는 곳
+  const completed = [];
 
-      if (endTime > nextStartTime) {
-        if (!tempStopped.has(name)) {
-          tempStopped.set(name, {
-            idx,
-            remain: requiredTime - getMinDiff(nextStartTime - startTime),
+  while (notStartedHomeworks.length > 0 || tempStoppedHomeWork.length > 0) {
+    if (onFocus.length === 0) {
+      //맨 처음인 경우
+      onFocus.push(notStartedHomeworks.pop());
+    } else {
+      //두번쨰...
+      //기존에 진행중이던 과제가 끝나는 시각, 새로운 과제 시작 시각을 비교
+      //a. 전자가 더 일찍 끝나는 경우: completed에 푸시
+      //b. 후자를 바로 시작해야 하는 경우: temp에 전 과제를 넣고, 남은 시각 업뎃해서 넣어주기, 그리고
+      const {
+        startTime: currentHomeWorkStartTime,
+        endTime: currentHomeWorkEndTime,
+      } = onFocus[0];
+      const { startTime: newHomeWorkStartTIme, endTime: newHomeWorkEndTIme } =
+        notStartedHomeworks[0];
+
+      if (currentHomeWorkEndTime <= newHomeWorkStartTIme) {
+        //새로 시작할 과제와 잠시 멈춘 과제가 모두 있는 경우
+        if (notStartedHomeworks.length > 0 && tempStoppedHomeWork.length > 0) {
+          completed.push(onFocus.shift());
+          onFocus.push(notStartedHomeworks.shift());
+        }
+
+        //잠시 멈춘 과제만 있는 경우
+        if (
+          notStartedHomeworks.length === 0 &&
+          tempStoppedHomeWork.length > 0
+        ) {
+          completed.push(onFocus.shift());
+          onFocus.push({
+            ...tempStoppedHomeWork.pop(),
+            startTime: newHomeWorkEndTIme,
           });
-        } else {
-          //이미 배열에 있는 경우, 시간 다시 계산해준다
-          const idx = tempStopped.get(name).idx;
-          const remain = tempStopped.get(name).remain;
-          tempStopped.set(name, {
-            idx,
-            remain: remain - getMinDiff(nextStartTime - startTime),
-          });
         }
-      }
-      //제시간에 끝낸경우
-      if (endTime < nextStartTime) {
-        answer.push(name);
-        //잠시 멈춰둔 과제가 있다면 이어서 진행
-        if (tempStopped.size > 0) {
-          const recentStoppped = Array.from(tempStopped).pop();
-          console.log(recentStoppped);
-        }
-      }
 
-      if (endTime === nextStartTime) {
-        answer.push(name);
-        //잠시 멈춰둔 과제가 있다면 새로 시작하는 과제부터 진행
-        if (tempStopped.size > 0) {
+        //새로 시작할 과제만 있는 경우
+        //정상적으로 끝남
+        if (
+          notStartedHomeworks.length > 0 &&
+          tempStoppedHomeWork.length === 0
+        ) {
+          completed.push(onFocus.shift());
+          onFocus.push(notStartedHomeworks.shift());
         }
+      } else {
+        //새 숙제부터 시작해야해
+        const { name, startTime, duration, endTime } = onFocus[0];
+        const newDuration =
+          duration - (newHomeWorkStartTIme - currentHomeWorkStartTime);
+        tempStoppedHomeWork.push({ ...onFocus.shift(), duration: newDuration });
+        onFocus.push(notStartedHomeworks.shift());
       }
     }
-  });
+  }
 
-  return answer;
+  return completed.map((plan) => plan.name);
 }
 
 function formatPlans(plans) {
   return plans.map((plan) => {
     const startTime = plan[1];
     const [hh, mm] = startTime.split(":");
-    return [plan[0], new Date(null, null, null, hh, mm), plan[2]];
+    return {
+      name: plan[0],
+      startTime: new Date(null, null, null, hh, mm),
+      durations: plan[2],
+      endTime: new Date(
+        new Date(null, null, null, hh, mm).getTime() + plan[2] * 60000
+      ),
+    };
   });
 }
 
-function getMinDiff(nextDate, date) {
-  return (((nextDate - date) % 86400000) % 3600000) / 60000;
-}
+const answer = solution([
+  ["korean", "11:40", "30"],
+  ["english", "12:10", "20"],
+  ["math", "12:30", "40"],
+]);
+
+console.log(answer);
+
+const answer2 = solution([
+  ["science", "12:40", "50"],
+  ["music", "12:20", "40"],
+  ["history", "14:00", "30"],
+  ["computer", "12:30", "100"],
+]);
+
+console.log(answer2);
+
+const answer3 = solution([
+  ["aaa", "12:00", "20"],
+  ["bbb", "12:10", "30"],
+  ["ccc", "12:40", "10"],
+]);
+
+console.log(answer3);
