f6cf59b51ff7e2cd62a6f8922f947e0833aa69f9
shjhwoo <shjhwoo@trustnhope.com>
2023-05-29 22:39:41 +0900

    2차 시도

--- solution.js
+++ solution.js
@@ -11,47 +11,34 @@ const personalities = {
 
 function solution(survey, choices) {
   let answer = "";
-  //성격 유형별로 점수를 합산하는 객체를 하나 만들고
 
-  //설문 배열과 선택한 답지를 정리해서 객체 만들고
-  const surveyChoices = {};
-  survey.forEach((element, idx) => {
-    surveyChoices[element] = choices[idx];
-  });
-  //이 객체를 순회하면서
-  // 성격 유형에다가 점수를 합산한다. (질문지, 선택지 )
-  for (const quest in surveyChoices) {
-    getPersonalityScore(quest, surveyChoices[quest]);
-  }
+  const mbti = {
+    R: 0,
+    T: 0,
+    C: 0,
+    F: 0,
+    J: 0,
+    M: 0,
+    A: 0,
+    N: 0,
+  };
 
-  const personalitiesArr = Object.entries(personalities);
-
-  personalitiesArr.forEach((personalArr, idx) => {
-    if (idx % 2 === 0) {
-      const score1 = personalArr[1];
-      const score2 = personalitiesArr[idx + 1][1];
-
-      if (score1 >= score2) {
-        answer += personalArr[0];
-      } else {
-        answer += personalitiesArr[idx + 1][0];
-      }
+  survey.forEach((q, idx) => {
+    const [left, right] = q.split("");
+    if (choices[idx] > 4) {
+      mbti[right] += choices[idx] - 4;
+    } else if (choices[idx] < 4) {
+      mbti[left] += 4 - choices[idx];
     }
   });
 
+  answer = answer.concat(mbti["R"] >= mbti["T"] ? "R" : "T");
+  answer = answer.concat(mbti["C"] >= mbti["F"] ? "C" : "F");
+  answer = answer.concat(mbti["J"] >= mbti["M"] ? "J" : "M");
+  answer = answer.concat(mbti["A"] >= mbti["N"] ? "A" : "N");
+
   return answer;
 }
-
-function getPersonalityScore(quest, choice) {
-  console.log(quest, choice, Math.abs(choice - 4));
-
-  if (choice - 4 > 0) {
-    personalities[quest[1]] += choice - 4;
-  } else if (choice - 4 < 0) {
-    personalities[quest[0]] += Math.abs(choice - 4);
-  }
-}
-
 //console.log(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]));
 
 console.log(solution(["TR", "RT", "TR"], [7, 1, 3]));
