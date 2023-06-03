89e648e6b11bbb94f0cd5c1ec8ccff5f2da6177d
shjhwoo <shjhwoo@trustnhope.com>
2023-06-03 11:29:06 +0900

    신고결과갑ㄷ기

--- solution.js
+++ solution.js
@@ -1,44 +1,39 @@
-const personalities = {
-  R: 0,
-  T: 0,
-  C: 0,
-  F: 0,
-  J: 0,
-  M: 0,
-  A: 0,
-  N: 0,
-};
+function solution(id_list, report, k) {
+  //중복 신고한 결과 제거
+  const trimmedReport = Array.from(new Set(report));
+  const reportedusers = trimmedReport.map((rep) => rep.split(" ")[1]);
 
-function solution(survey, choices) {
-  let answer = "";
+  //유저 - 정지여부를 정리한 객체 선언
+  const isBlocked = {};
+  id_list.forEach((id) => {
+    isBlocked[id] =
+      reportedusers.filter((reporteduser) => reporteduser === id).length >= k;
+  });
+  console.log(isBlocked, "각 유저별로 정지 여부 확인합니다");
 
-  const mbti = {
-    R: 0,
-    T: 0,
-    C: 0,
-    F: 0,
-    J: 0,
-    M: 0,
-    A: 0,
-    N: 0,
-  };
-
-  survey.forEach((q, idx) => {
-    const [left, right] = q.split("");
-    if (choices[idx] > 4) {
-      mbti[right] += choices[idx] - 4;
-    } else if (choices[idx] < 4) {
-      mbti[left] += 4 - choices[idx];
+  //유저 - 각 유저가 신고한 사람 배열 정리
+  const reporter_targetList = {};
+  trimmedReport.forEach((rep) => {
+    const [reporter, target] = rep.split(" ");
+    if (reporter_targetList[reporter] === undefined) {
+      reporter_targetList[reporter] = isBlocked[target] ? 1 : 0;
+    } else {
+      const add = isBlocked[target] ? 1 : 0;
+      reporter_targetList[reporter] = reporter_targetList[reporter] + add;
     }
   });
 
-  answer = answer.concat(mbti["R"] >= mbti["T"] ? "R" : "T");
-  answer = answer.concat(mbti["C"] >= mbti["F"] ? "C" : "F");
-  answer = answer.concat(mbti["J"] >= mbti["M"] ? "J" : "M");
-  answer = answer.concat(mbti["A"] >= mbti["N"] ? "A" : "N");
+  console.log(reporter_targetList, "각 유저별로 누구 신고했는지 확인합니다");
 
-  return answer;
+  return Object.values(reporter_targetList);
 }
-//console.log(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]));
 
-console.log(solution(["TR", "RT", "TR"], [7, 1, 3]));
+//목표
+/*
+내가 다른 사람을 신고했을 떄 정지된 이용자에 대해서만 신고 결과를 받게 되는데
+그런 신고 결과를 받은 횟수를 배열에 담아서 리턴한다
+
+한 유저가 같은 유저를 여러 번 신고할 수도 있지만 그 경우는 한번 신고한 것으로 간주하기 떄문에 report 배열을 
+셋으로 만들어서 중복을 제거해버리기.
+
+*/
