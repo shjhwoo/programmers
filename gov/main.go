3b84e60069b90f60bf466075507a3c0efde4e2e8
shjhwoo <shjhwoo@trustnhope.com>
2023-07-08 10:18:16 +0900

    하노이의 탑

--- gov/main.go
+++ gov/main.go
@@ -2,29 +2,51 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Println(solution(2000))
-	fmt.Println(solution(3))
+	fmt.Println(solution(15))
+	fmt.Println(solution(1))
 }
 
-var cache = make(map[int]int64)
+func solution(n int) [][]int {
+	// if n == 1 {
+	// 	return [][]int{{1, 3}}
+	// }
 
-func solution(n int) int64 {
-	cache[0] = 0
+	// if n == 2 {
+	// 	return [][]int{{1,2},{1,3},{2,3}}
+	// }
 
-	if n == 1 || n == 2 {
-		cache[n] = int64(n)
-		return int64(n % 1234567)
+	//홀수일때
+	//짝수일때
+	//배열의길이 = 이동하는 최소횟수
+	//이동하는 최소횟수는 2pow n - 1
+
+	answer := [][]int{{}}
+	if isEven(n) {
+		answer = append(answer, []int{1, 2})
+	} else {
+		answer = append(answer, []int{1, 3})
 	}
 
-	if cache[n] != 0 {
-		return int64(cache[n] % 1234567)
+	movNum := math.Pow(2, float64(n)) - 1
+	for len(answer) < int(movNum) {
+
 	}
 
-	ans := (solution(n-2) + solution(n-1))
-	cache[n] = ans
-
-	return cache[n] % 1234567
+	return answer
 }
+
+func isEven(n int) bool {
+	return n%2 == 0
+}
+
+/*
+1,3
+1,2 -> 1,3 -> 2,3
+1,3 -> 1,2 -> 3,2 -> 1,3 -> 2,1 -> 2,3 -> 1,3 : (1,2) 3 이 2개의 원판을 1-> 3으로 이동하는 방법임
+1,2 -> 1,3 -> 2,3 -> 1,2 -> 3,1 -> 3,2 -> 1,2 -> 1,3 -> 2,3 -> 2,1 -> 3,1 -> 2,3 -> 1,2 -> 1,3 -> 2,3
+1,3 -> 1,2 -> 3,2 -> 1,3 -> 2,1 -> 2,3 -> 1,3 -> ...
+*/
