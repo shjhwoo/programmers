7845aef5a2c753128e92918f6a7f9e502fc6309d
shjhwoo <shjhwoo@trustnhope.com>
2023-07-16 12:06:47 +0900

    하노이탑 수도코드까지만 정리, 2차시도

--- gov/main.go
+++ gov/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
@@ -11,9 +10,10 @@ func main() {
 }
 
 func solution(n int) [][]int {
-	// if n == 1 {
-	// 	return [][]int{{1, 3}}
-	// }
+	//
+	if n == 1 {
+		return [][]int{{1, 3}}
+	}
 
 	// if n == 2 {
 	// 	return [][]int{{1,2},{1,3},{2,3}}
@@ -25,28 +25,40 @@ func solution(n int) [][]int {
 	//이동하는 최소횟수는 2pow n - 1
 
 	answer := [][]int{{}}
-	if isEven(n) {
-		answer = append(answer, []int{1, 2})
-	} else {
-		answer = append(answer, []int{1, 3})
-	}
-
-	movNum := math.Pow(2, float64(n)) - 1
-	for len(answer) < int(movNum) {
-
-	}
 
 	return answer
 }
 
-func isEven(n int) bool {
-	return n%2 == 0
+// func isEven(n int) bool {
+// 	return n%2 == 0
+// }
+
+func buildLeftRoute() [][]int {
+
 }
 
-/*
-1,3
-1,2 -> 1,3 -> 2,3
-1,3 -> 1,2 -> 3,2 -> 1,3 -> 2,1 -> 2,3 -> 1,3 : (1,2) 3 이 2개의 원판을 1-> 3으로 이동하는 방법임
-1,2 -> 1,3 -> 2,3 -> 1,2 -> 3,1 -> 3,2 -> 1,2 -> 1,3 -> 2,3 -> 2,1 -> 3,1 -> 2,3 -> 1,2 -> 1,3 -> 2,3
-1,3 -> 1,2 -> 3,2 -> 1,3 -> 2,1 -> 2,3 -> 1,3 -> ...
+func buildMiddle(start, end int) []int {
+	return []int{start, end}
+}
+
+func buildRightRoute() [][]int {
+
+}
+
+/*규칙성
+
+f(1,3) = f(1,2) + (1,3) + f(2,3)
+f(1,2) = f(1,3) + (1,2) + f(3,2)
+f(2,3) = f(2,1) + (2,3) + f(1,3)
+f(3,2) = f(3,1) + (3,2) + f(1,2)
+f(2,1) = f(2,3) + (2,1) + f(3,1)
+
+정리하자면
+맨 밑의 판을 제외한 그 위의 나머지 판들을 목적지와는 다른 곳으로 이동을 시키고
+그다음 맨 밑의 판을 목적지로 이동시키고
+다른 곳으로 잠시 이동시켰던 나머지 판들을 다시 목적지로 이동시키는 방법과 같다
+
+f 함수를 호출할 때 이동해야 하는 판의 개수가 1개뿐이면은 LR은 무시한다
+
+즉 f(시작정, 도착점) = f(시작 , 도착아닌곳) + (시작, 도착) + f(도착아닌곳, 도착)
 */
