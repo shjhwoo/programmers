b8f66e17c1316606b5334d16bcbe102f07539e31
sue hyun kim <108392916+shjhwoo@users.noreply.github.com>
2023-07-22 10:23:59 +0900

    하노이의 탑 통과

--- gov/main.go
+++ gov/main.go
@@ -5,44 +5,48 @@ import (
 )
 
 func main() {
-	fmt.Println(solution(15))
-	fmt.Println(solution(1))
+	// fmt.Println(solution(15))
+	fmt.Println(solution(3))
 }
+
+// 키: start-end-panelNum 이렇게 만들자!!
+var cache = map[string][][]int{}
 
 func solution(n int) [][]int {
-	//
-	if n == 1 {
-		return [][]int{{1, 3}}
+	return move(1, 3, n)
+}
+
+func move(start, end, panelNum int) [][]int {
+	k := fmt.Sprintf("%d-%d-%d", start, end, panelNum)
+	if panelNum == 1 {
+		cache[k] = [][]int{{start, end}}
 	}
 
-	// if n == 2 {
-	// 	return [][]int{{1,2},{1,3},{2,3}}
-	// }
+	if cache[k] != nil {
+		return cache[k]
+	}
 
-	//홀수일때
-	//짝수일때
-	//배열의길이 = 이동하는 최소횟수
-	//이동하는 최소횟수는 2pow n - 1
+	op := findOtherPillar(start, end)
 
-	answer := [][]int{{}}
+	left := move(start, op, panelNum-1)
+	middle := [][]int{{start, end}}
+	right := move(op, end, panelNum-1)
 
-	return answer
+	result := [][]int{}
+	result = append(append(append(result, left...), middle...), right...)
+	cache[k] = result
+
+	return result
 }
 
-// func isEven(n int) bool {
-// 	return n%2 == 0
-// }
-
-func buildLeftRoute() [][]int {
-
-}
-
-func buildMiddle(start, end int) []int {
-	return []int{start, end}
-}
-
-func buildRightRoute() [][]int {
-
+func findOtherPillar(start, end int) int {
+	p := []int{1, 2, 3}
+	for _, item := range p {
+		if item != start && item != end {
+			return item
+		}
+	}
+	return 0
 }
 
 /*규칙성
@@ -61,4 +65,6 @@ f(2,1) = f(2,3) + (2,1) + f(3,1)
 f 함수를 호출할 때 이동해야 하는 판의 개수가 1개뿐이면은 LR은 무시한다
 
 즉 f(시작정, 도착점) = f(시작 , 도착아닌곳) + (시작, 도착) + f(도착아닌곳, 도착)
+
+재귀 종료 조건: 이동해야 하는 판의 개수가 한개뿐일때 재귀조건은 종료된다,
 */
