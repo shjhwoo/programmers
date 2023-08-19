69be5fb2be73b76d91092033cb916b91f3bff98c
shjhwoo <shjhwoo@trustnhope.com>
2023-08-19 11:29:50 +0900

    행렬의 곱셈

--- gov/main.go
+++ gov/main.go
@@ -1,92 +1,39 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	var result = Solution([]string{"XXX", "XXX", "XXX", "XXX"})
+	var result = Solution([][]int{{1, 4}, {3, 2}, {4, 1}}, [][]int{{3, 3}, {3, 3}})
 	fmt.Println("최종 결과값", result)
 
-	var res2 = Solution([]string{"X591X", "X1X5X", "X231X", "1XXX1"})
-	fmt.Println(res2, "두번째 테스트")
+	var result2 = Solution([][]int{{2, 3, 2}, {4, 2, 4}, {3, 1, 4}}, [][]int{{5, 4, 3}, {2, 4, 1}, {3, 1, 1}})
+	fmt.Println("최종 결과값", result2)
 }
 
-var directions = [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
-
-func Solution(maps []string) []int {
-	//주어진 맵에 전부다 X밖에 없는 상황이면은..
-	if hasNoIsland(maps) {
-		return []int{-1}
-	}
-
-	//그 외의 상황에서는 각 섬에서 최대 며칠 머무를 수 있는지 알아야 한다.
-
-	//map을 쪼개준다
-	pixmap := getPixeledMap(maps)
-	result := []int{}
-
-	for r, row := range pixmap {
-		for c, col := range row {
-			//탐색가능한 지점이다(완전히 새로운 섬이다!!)
-			if col != "X" && col != "0" {
-				food, _ := strconv.Atoi(col)
-				func() {
-					var dfs func(int, int)
-					dfs = func(startRow, startCol int) {
-						pixmap[r][c] = "0"
-						for _, d := range directions {
-							newSpot := []int{r + d[0], r + d[1]}
-							if isValidSpot(pixmap, newSpot[0], newSpot[1]) {
-								colInt, _ := strconv.Atoi(pixmap[newSpot[0]][newSpot[1]])
-								food += colInt
-								pixmap[newSpot[0]][newSpot[1]] = "0"
-								dfs(newSpot[0], newSpot[1])
-							}
-						}
-					}
-					dfs(r, c)
-				}()
-				result = append(result, food)
+func Solution(arr1 [][]int, arr2 [][]int) [][]int {
+	result := [][]int{}
+	for i := range arr1 {
+		resRow := []int{}
+		for h := 0; h < len(arr2[0]); h++ {
+			sum := 0
+			for j := range arr1[i] {
+				sum += arr1[i][j] * arr2[j][h]
+				//fmt.Println(i, "번째 줄일때: ", i, j, j, h, arr1[i][j], arr2[j][h])
 			}
+			resRow = append(resRow, sum)
 		}
+		result = append(result, resRow)
 	}
-
 	return result
 }
 
-func hasNoIsland(maps []string) bool {
-	for _, row := range maps {
-		if !isAllX(row) {
-			return false
-		}
-	}
-	return true
-}
+/*
+arr1의 원소 개수만큼 행이 생기고
+arr2에 있는 첫번째 원소의 원소개수만큼 열이생긴다
 
-func isAllX(row string) bool {
-	pList := strings.Split(row, "")
+1차 반복>
+arr1에 있는 원소 개수만큼 반복
 
-	for _, p := range pList {
-		if p != "X" {
-			return false
-		}
-	}
-	return true
-}
-
-func getPixeledMap(maps []string) [][]string {
-	result := [][]string{}
-	for _, row := range maps {
-		newRow := append([]string{}, strings.Split(row, "")...)
-		result = append(result, newRow)
-	}
-
-	return result
-}
-
-func isValidSpot(maps [][]string, row, col int) bool {
-	return row < len(maps) && col < len(maps[0]) && row >= 0 && col >= 0 && maps[row][col] != "0"
-}
+2차반복> arr1에 있는 첫번째 원소가 가진 원소 개수만큼 반복.
+	arr2의 원소의 각 n번째 요소랑 각각 곱한뒤 더해준다!
+*/
