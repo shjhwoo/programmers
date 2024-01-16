97e67e5c5865234ef2297854899b39d2a19d2abe
shjhwoo <shjhwoo@trustnhope.com>
2024-01-16 20:57:17 +0900

    두 정수 사이의 합

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -1,71 +1,67 @@
 package main_test
 
 import (
-	"slices"
-	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 type TestCase struct {
-	arr     []int
-	divisor int
-	expect  []int
+	input  [2]int
+	expect int64
 }
 
 func TestSolution(t *testing.T) {
 
 	var tests = []TestCase{
 		{
-			arr:     []int{5, 9, 7, 10},
-			divisor: 5,
-			expect:  []int{5, 10},
+			input:  [2]int{3, 5},
+			expect: 12,
 		},
 		{
-			arr:     []int{2, 36, 1, 3},
-			divisor: 1,
-			expect:  []int{1, 2, 3, 36},
+			input:  [2]int{3, 3},
+			expect: 3,
 		},
 		{
-			arr:     []int{3, 2, 6},
-			divisor: 10,
-			expect:  []int{-1},
+			input:  [2]int{5, 3},
+			expect: 12,
 		},
 	}
 
 	for _, test := range tests {
-		ans := solution(test.arr, test.divisor)
-		if !assert.True(t, slices.Equal(test.expect, ans)) {
-			t.Log(test.arr, ans)
+		ans := solution(test.input[0], test.input[1])
+		if !assert.Equal(t, test.expect, ans) {
+			t.Log(test.input, "에서 실패~")
 		}
 	}
 }
 
-func solution(arr []int, divisor int) []int {
-	if divisor == 1 {
-		sort.Slice(arr, func(i, j int) bool {
-			return arr[i] < arr[j]
-		})
-
-		return arr
+func solution(a int, b int) int64 {
+	if a == b {
+		return int64(a)
 	}
 
-	var answer []int
-
-	for _, num := range arr {
-		if num >= divisor && num%divisor == 0 {
-			answer = append(answer, num)
-		}
+	var numberOfInt int64
+	if a < b {
+		numberOfInt = int64(b - a + 1)
+	} else {
+		numberOfInt = int64(a - b + 1)
 	}
 
-	if len(answer) == 0 {
-		return []int{-1}
+	subSum := int64(a + b)
+	if numberOfInt%2 == 0 {
+		return subSum * (numberOfInt / 2)
 	}
 
-	sort.Slice(answer, func(i, j int) bool {
-		return answer[i] < answer[j]
-	})
+	midNum := int64((a + b) / 2)
+	return subSum*(numberOfInt/2) + midNum
 
-	return answer
+	/*
+		//a,b 경계를 합해서 나열한 숫자의 개수가 짝수일 때 합은
+		-2 -1 0 1 2 3 4 5 6 7
+
+		//홀수일 때 합은
+		-2 -1 0 1    2    3 4 5 6
+
+	*/
 }
