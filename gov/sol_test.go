88d8886f100dec588852b4d3dda482021319a3db
shjhwoo <shjhwoo@trustnhope.com>
2024-02-21 22:03:11 +0900

    오류 수정

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -9,43 +9,102 @@ import (
 )
 
 type TestCase struct {
-	array    []int
-	commands [][]int
-	expect   []int
+	stringSlice []string
+	num         int
+	expect      []string
 }
 
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
 		{
-			array:    []int{1, 5, 2, 6, 3, 7, 4},
-			commands: [][]int{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}},
-			expect:   []int{5, 6, 3},
+			stringSlice: []string{"sun", "bed", "car"},
+			num:         1,
+			expect:      []string{"car", "bed", "sun"},
+		},
+		{
+			stringSlice: []string{"abce", "abcd", "cdx"},
+			num:         2,
+			expect:      []string{"abcd", "abce", "cdx"},
 		},
 	}
 
 	for _, test := range tests {
-		ans := solution(test.array, test.commands)
+		ans := solution(test.stringSlice, test.num)
 		t.Log(ans, "계산값")
 		assert.True(t, slices.Equal(test.expect, ans))
 	}
 }
 
-func solution(array []int, commands [][]int) []int {
+var dict = map[string]int{
+	"a": 0,
+	"b": 1,
+	"c": 2,
+	"d": 3,
+	"e": 4,
+	"f": 5,
+	"g": 6,
+	"h": 7,
+	"i": 8,
+	"j": 9,
+	"k": 10,
+	"l": 11,
+	"m": 12,
+	"n": 13,
+	"o": 14,
+	"p": 15,
+	"q": 16,
+	"r": 17,
+	"s": 18,
+	"t": 19,
+	"u": 20,
+	"v": 21,
+	"w": 22,
+	"x": 23,
+	"y": 24,
+	"z": 25,
+}
 
-	//자른다
-	var answer []int
-	for _, command := range commands {
-		start := command[0] - 1
-		end := command[1]
+func solution(strings []string, n int) []string {
 
-		var sliced []int
-		part := array[start:end] //새로운 곳에 복사해야함
-		sliced = append(sliced, part...)
+	dictionary := strings
+	sort.Strings(dictionary)
 
-		//정렬 - 오름차순
-		sort.Ints(sliced)
-		answer = append(answer, sliced[command[2]-1])
+	dictMap := make(map[string]int)
+	for idx, word := range dictionary {
+		dictMap[word] = idx
 	}
 
-	return answer
+	sort.Slice(strings, func(i, j int) bool {
+		leftword := strings[i]
+		rightword := strings[j]
+
+		leftChrIdx := dict[leftword[n:n+1]]
+		rightChrIdx := dict[rightword[n:n+1]]
+
+		if leftChrIdx != rightChrIdx {
+			return leftChrIdx < rightChrIdx
+		}
+
+		//사전순으로 앞선 단어 먼저.
+		leftDictIdx := dictMap[leftword]
+		rightDictIdx := dictMap[rightword]
+		return leftDictIdx < rightDictIdx
+	})
+
+	return strings
 }
+
+// 문자끼리 대소비교 가능..
+// func solution(strings []string, n int) []string {
+
+// 	sort.Slice(strings, func(i, j int) bool {
+// 		strA := []byte(strings[i])
+// 		strB := []byte(strings[j])
+// 		if strA[n] == strB[n] {
+// 			return strings[i] < strings[j]
+// 		}
+// 		return strA[n] < strB[n]
+// 	})
+
+// 	return strings
+// }
