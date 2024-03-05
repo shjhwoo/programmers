2a6a6d04e64e2e3bf1d036e28bc58e19f195f54f
shjhwoo <shjhwoo@trustnhope.com>
2024-03-05 22:39:20 +0900

    명예의 전당

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -4,65 +4,113 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/tools/go/pointer"
 )
 
 type TestCase struct {
-	a      int
-	b      int
-	n      int
-	expect int
+	k      int
+	score  []int
+	expect []int
 }
 
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
 		{
-			a:      2,
-			b:      1,
-			n:      20,
-			expect: 19,
+			k:      3,
+			score:  []int{10, 100, 20, 150, 1, 100, 200},
+			expect: []int{10, 10, 10, 20, 20, 100, 100},
 		},
 		{
-			a:      3,
-			b:      1,
-			n:      20,
-			expect: 9,
-		},
-		{
-			a:      7,
-			b:      2,
-			n:      20,
-			expect: 6, //(20-14+4) = 10 => (10-7+2) = 5  , 4 + 2
+			k:      4,
+			score:  []int{0, 300, 40, 300, 20, 70, 150, 50, 500, 1000},
+			expect: []int{0, 0, 0, 0, 20, 40, 70, 70, 150, 300},
 		},
 	}
 
 	for _, test := range tests {
-		ans := solution(test.a, test.b, test.n)
+		ans := solution(test.k, test.score)
 		t.Log(ans, "계산값")
 		assert.Equal(t, test.expect, ans)
 	}
 }
 
-func solution(a int, b int, n int) int {
-	//얼마만큼을 반복해야 하는지가 문제이다.
+func solution(k int, score []int) []int {
+	var answer []int
+	var podium = initPodium(score[:k])
 
-	//a개만큼의 빈병을 주면 콜라 b병을 받는다.
-	//처음에 가지고 있는 빈병은 n개이다.
+	//처음 k개까지는, 가장 낮은 거 동일하ㅔ 채워넣는다.
+	lowestScoreOfKdays := getLowestScore(score[:k])
+	for len(answer) < k {
+		answer = append(answer, lowestScoreOfKdays)
+	}
 
-	// n개의 병을 a로 나눈다. => 몫 : 받을 수 있는 콜라의 수,
-	// 이후 가지게 되는 콜라의 수 => 나머지 + 몫 .... 이 값을 x라고 하자.
-
-	// 그러면 x가 a보다 작으면 이때 정답을 반환하면 된다
-
-	//반복은 x가 a보다 크거나, 같은 동안만 할 수 있다.
-
-	var answer = (n / a) * b
-
-	var numberOfColaBottles = (n % a) + (n/a)*b
-
-	for numberOfColaBottles >= a {
-		answer += (numberOfColaBottles / a) * b
-		numberOfColaBottles = (numberOfColaBottles % a) + (numberOfColaBottles/a)*b
+	//그 다음부터는 1명씩 갈아치워야 한다.
+	for i := k; i < len(score); i++ {
+		answer = append(answer,  getLowestScore(podium))
+		podium = updatePodium(podium, score[i])
 	}
 
 	return answer
+}
+
+func getLowestScore(scores []int) int {
+	lowest := scores[0]
+	for _, v := range scores {
+		if v < lowest {
+			lowest = v
+		}
+	}
+	return lowest
+}
+
+func initPodium(scoresOfKdays []int) []int {
+
+	var podium = []int{}
+	podium[0] = scoresOfKdays[0]
+
+
+	leftIdx := 0
+	rightIdx := len(scoresOfKdays) - 1
+
+	for i := 1; i < len(scoresOfKdays); i++ {
+		firstVal := scoresOfKdays[0]
+		lastVal := podium[i-1]
+
+		//가운데 끼는 값인경우
+		if firstVal < scoresOfKdays[i] && scoresOfKdays[i] < lastVal {
+
+		}
+
+		//firstVal보다도 작은경우
+		if scoresOfKdays[i] =< firstVal {
+			podium = append([]int{scoresOfKdays[i]}, podium...)
+			continue
+		}
+
+		//lastVal보다도 큰경우
+		if scoresOfKdays[i] >= lastVal {
+			podium = append(podium, scoresOfKdays[i])
+			continue
+		}
+	}
+
+	return podium
+}
+
+
+func updatePodium(podium []int, score int) []int {
+	lowestScore := podium[0]
+	highestScore := podium[len(podium)-1]
+
+	if score > highestScore {
+		podium = append(podium, score)
+		return podium[1:]
+	}
+
+	if score < lowestScore {
+		return podium
+	}
+
+	//가운데에 끼는 경우..
+	
 }
