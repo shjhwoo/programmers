5fad4997d1525aedc099632987a2febd008dce5f
shjhwoo <shjhwoo@trustnhope.com>
2024-03-28 21:40:12 +0900

    문자열 나누기

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -7,89 +7,83 @@ import (
 )
 
 type TestCase struct {
-	lottos   [6]int
-	win_nums [6]int
-	expect   []int
+	s      string
+	expect int
 }
 
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
+		// {
+		// 	s:      "banana",
+		// 	expect: 3,
+		// },
+		// {
+		// 	s:      "abracadabra",
+		// 	expect: 6,
+		// },
+		// {
+		// 	s:      "aaabbaccccabba",
+		// 	expect: 3,
+		// },
 		{
-			lottos:   [6]int{44, 1, 0, 0, 31, 25},
-			win_nums: [6]int{31, 10, 45, 1, 6, 19},
-			expect:   []int{3, 5},
-		},
-		{
-			lottos:   [6]int{0, 0, 0, 0, 0, 0},
-			win_nums: [6]int{38, 19, 20, 40, 15, 25},
-			expect:   []int{1, 6},
-		},
-		{
-			lottos:   [6]int{45, 4, 35, 20, 3, 9},
-			win_nums: [6]int{20, 9, 3, 45, 4, 35},
-			expect:   []int{1, 1},
+			s:      "zzzzzz",
+			expect: 1,
 		},
 	}
 
 	for _, test := range tests {
-		ans := solution(test.lottos, test.win_nums)
+		ans := solution(test.s)
 		t.Log(ans, "계산값")
 		assert.DeepEqual(t, test.expect, ans)
 	}
 }
 
-func solution(lottos [6]int, win_nums [6]int) []int {
-	win_numMap := make(map[int]bool)
-	for _, num := range win_nums {
-		win_numMap[num] = true
-	}
+func solution(s string) int {
 
-	var answer []int
+	var answer int
 
-	var baseHitCount int
-	var matchedNumberMap = make(map[int]bool)
-	var numverOfZero int
-	for _, num := range lottos {
-		if _, exist := win_numMap[num]; exist {
-			baseHitCount++
-			matchedNumberMap[num] = true
+	var xCount int
+	var otherCount int
+
+	var x rune
+
+	runes := []rune(s)
+	for index, char := range runes {
+		if xCount == 0 && otherCount == 0 {
+			x = char
+			xCount++
+
+			if index == len(runes)-1 {
+				answer++
+			}
+			continue
 		}
 
-		if num == 0 {
-			numverOfZero++
+		if x != char {
+			otherCount++
+
+			if otherCount == xCount {
+				answer++
+				xCount = 0
+				otherCount = 0
+			}
+
+			if otherCount != xCount && index == len(runes)-1 {
+				answer++
+				break
+			}
 		}
-	}
 
-	var unmatchedNumberMap = make(map[int]bool)
-	for _, wnum := range win_nums {
-		if _, exist := matchedNumberMap[wnum]; !exist {
-			unmatchedNumberMap[wnum] = true
+		if x == char {
+			xCount++
+
+			if index == len(runes)-1 {
+				answer++
+				continue
+			}
 		}
-	}
 
-	//최저 순위: 로또구매자가 선택한 번호 중 0을 제외하고 당첨번호와 일치하는 개수를 센다.
-	var lowestRank int
-	if baseHitCount < 2 {
-		lowestRank = 6
-	} else {
-		lowestRank = 7 - baseHitCount
 	}
-
-	var highestRank int
-	//최고 순위) 0으로 되어있는 부분.
-	if numverOfZero == 0 || len(unmatchedNumberMap) == 0 {
-		highestRank = lowestRank
-	} else {
-		hitCount := (baseHitCount + numverOfZero)
-		if hitCount < 2 {
-			highestRank = 6
-		} else {
-			highestRank = 7 - hitCount
-		}
-	}
-
-	answer = append(answer, highestRank)
-	answer = append(answer, lowestRank)
 
 	return answer
 }
