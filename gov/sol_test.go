e4d80b0c819ae9e8fe559d957b706ee325ca0a85
shjhwoo <shjhwoo@trustnhope.com>
2024-04-11 22:17:50 +0900

    햄버거만들기 통과

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -17,22 +17,22 @@ func TestSolution(t *testing.T) {
 			ingredient: []int{2, 1, 1, 2, 3, 1, 2, 3, 1},
 			expect:     2,
 		},
-		// {
-		// 	ingredient: []int{1, 3, 2, 1, 2, 1, 3, 1, 2},
-		// 	expect:     0,
-		// },
-		// {
-		// 	ingredient: []int{1, 2, 3, 1, 1, 2, 3, 1, 1, 2, 3, 1},
-		// 	expect:     3,
-		// },
-		// {
-		// 	ingredient: []int{1, 2, 1, 2, 1, 2, 1, 2, 3, 1, 3, 1, 3, 1, 3, 1},
-		// 	expect:     4,
-		// },
-		// {
-		// 	ingredient: []int{1, 2, 1, 2, 3, 1, 3, 1, 2, 3, 1, 1},
-		// 	expect:     2,
-		// },
+		{
+			ingredient: []int{1, 3, 2, 1, 2, 1, 3, 1, 2},
+			expect:     0,
+		},
+		{
+			ingredient: []int{1, 2, 3, 1, 1, 2, 3, 1, 1, 2, 3, 1},
+			expect:     3,
+		},
+		{
+			ingredient: []int{1, 2, 1, 2, 1, 2, 1, 2, 3, 1, 3, 1, 3, 1, 3, 1},
+			expect:     4,
+		},
+		{
+			ingredient: []int{1, 2, 1, 2, 3, 1, 3, 1, 2, 3, 1, 1},
+			expect:     2,
+		},
 		//들어온 순서대로 1231이 완성되면 햄버거가 완성되어야하는데 전체에서 따지면 순서대로가아닌 초기상태에서 만든후 빠집니다.
 	}
 
@@ -60,12 +60,12 @@ func solution(ingredient []int) int {
 		}
 
 		if ingredient[index] == 1 {
-			if ingredient[index-1] < 3 {
+			if index >= 1 && ingredient[index-1] < 3 {
 				index = index + 3
 				continue
 			}
 
-			if isAbleToWrap(ingredient[index-3 : index]) {
+			if index >= 3 && isAbleToWrap(ingredient[index-3:index]) {
 				answer++
 				ingredient = append(ingredient[:index-3], ingredient[index+1:]...)
 				index = index - 3
