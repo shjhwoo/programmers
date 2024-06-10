a59434003751eb52127ca3a3c9d896fe987a32a6
shjhwoo <shjhwoo@trustnhope.com>
2024-06-10 22:15:35 +0900

    중간결과

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -57,61 +57,22 @@ func solution(n int, left int64, right int64) []int {
 		left에서 right 까지 차지하는 칸 수를 세본다 = right - left + 1 개의 숫자가 들어간다.
 		=> answer의 길이가 right - left + 1 이 될때까지 for 문을 실행시키고,
 		for 문 안에서 다음 작업을 진행한다
-
-
 	*/
 
-	leftRow, leftCol := findLeftRowCol(n, left)
-
 	var answer []int
-	answer = append(answer, leftCol)
-
-	goalLength := int(right - left + 1)
-
-	for len(answer) < goalLength {
-		/*
-			경우의 수를 생각
-			leftCol 우선 기준으로 했을 때,
-
-			leftCol이 n과 동일하다면 다음 행으로 넘어가야 한다.
-				=> 다음 행 채우기: leftRow값을 보고 결정해야 한다
-				leftRow + 1 로 된 숫자로 그만큼 반복해서 채운다. (길이가 n이 될때까지)
-				만약에 모자라면 1씩 증가시키면서 1칸씩 채운다.
-
-			leftCol이 n보다 작다면 leftRow가 일단 몇번째 행인지를 확인한다.
-
-
-			X행 Y열인 경우, Y값이 X보다 작거나 같으면 X값을 그대로 쓰면 된다. 그 외에는 1씩 증가시키며 채운다
-
-		*/
-
-		if leftCol == n {
-
-			var filler []int
-
-			for len(filler) < n {
-				for i := 0; i < leftRow+1; i++ {
-					filler = append(filler, leftRow+1)
-				}
-
-				if leftRow+1 < n {
-					//모자란 만큼 더 채워야 한다. 길이가 n이 될때까지(마지막에 채우는 숫자가 n이 될때까지)
-					var fillNum = leftRow + 2
-
-				}
-			}
-
-		}
-
-		if leftCol < n {
-		}
-
+	for i := left; i <= right; i++ {
+		row, col := findRowColFromIdx(n, i)
+		answer = append(answer, findNumByRowCol(row, col))
 	}
 
-	return []int{}
+	return answer
 }
 
-func findLeftRowCol(n int, left int64) (int, int) {
+//행과 열 정보를 주면 그 자리에 들어가야 하는 숫자가 뭔지 알아내면 되고, 그 규칙을 만들면 된다
+//left 의 행과 열 정보부터 파악 => ... => right의 행과 열 정보 파악한다.
+//그 사이에 있는 값들의 행과 열 정보를 파악 => 그 사이 숫자들에 대한
+
+func findRowColFromIdx(n int, left int64) (int, int) {
 	var rowNo int
 	var columnNo int
 
@@ -126,4 +87,20 @@ func findLeftRowCol(n int, left int64) (int, int) {
 	}
 
 	return rowNo, columnNo
+}
+
+func findNumByRowCol(row int, col int) int {
+	if row == col {
+		return row
+	}
+
+	if row > col {
+		return row
+	}
+
+	if row < col {
+		return col
+	}
+
+	return 0
 }
