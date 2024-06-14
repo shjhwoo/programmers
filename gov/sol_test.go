8b0bea8c45bf7d8d6c8c7ed6466dcdf8ce83231d
shjhwoo <shjhwoo@trustnhope.com>
2024-06-14 21:58:00 +0900

    뒤에 있는 큰 수 찾기

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -7,91 +7,72 @@ import (
 )
 
 type TestCase struct {
-	progresses []int
-	speeds     []int
-	expect     []int
+	numbers []int
+	expect  []int
 }
 
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
 		{
-			progresses: []int{93, 30, 55},
-			speeds:     []int{1, 30, 5},
-			expect:     []int{2, 1},
+			numbers: []int{2, 3, 3, 5},
+			expect:  []int{3, 5, 5, -1},
 		},
 		{
-			progresses: []int{95, 90, 99, 99, 80, 99},
-			speeds:     []int{1, 1, 1, 1, 1, 1},
-			expect:     []int{1, 3, 2},
-		},
-		{
-			progresses: []int{90, 91, 92, 93, 94, 95},
-			speeds:     []int{1, 1, 1, 1, 1, 1}, // 10, 9, 8, 7, 6, 5
-			expect:     []int{6},
-		},
-		{
-			progresses: []int{90, 91, 92, 93, 94, 95},
-			speeds:     []int{1, 1, 1, 1, 1, 1}, // 10, 9, 8, 7, 6, 5
-			expect:     []int{6},
-		},
-		{
-			progresses: []int{90, 91, 92, 93, 94, 90},
-			speeds:     []int{1, 1, 1, 1, 1, 1}, // 10, 9, 8, 7, 6, 10
-			expect:     []int{6},
+			numbers: []int{9, 1, 5, 3, 6, 2},
+			expect:  []int{-1, 5, 6, 6, -1, -1},
 		},
 	}
 
 	for _, test := range tests {
-		ans := solution(test.progresses, test.speeds)
+		ans := solution(test.numbers)
 		t.Log(ans, "계산값")
 		assert.DeepEqual(t, test.expect, ans)
 	}
 }
 
-func solution(progresses []int, speeds []int) []int {
+func solution(numbers []int) []int {
+	var answer = make([]int, len(numbers))
+	/*
+		기본로직
 
-	//일단 배열을 돌면서 작업 완료까지 필요한 기간을 계산해본다
-	var remainDaysForDeploy []int
-	for i := 0; i < len(progresses); i++ {
-		rm := (100 - progresses[i]) % speeds[i]
-		daysNeed := (100 - progresses[i]) / speeds[i]
-		if rm > 0 {
-			daysNeed++
+		배열 순회는 해야한다
+		자기 바로 뒤에 있는 숫자부터 비교를 시작한다 (최대한 빨리 탐색을 끝내는 것이 좋음)
+
+		뒷 큰수가 자기 바로 뒤 말고 몇칸 떨어져서 있다면,
+
+		자기 자신과 뒷 큰 수 사이에 있는 아이들은
+		자기 자신보다는 분명히 작거나 같음을 알 수 있다.
+		그리고 이 사이에 있는 아이들은 뒷 큰수보다도 작음.
+		(이 범위에서만 비교를 하면 되는 것이다)
+		그러면 배열의 끝까지 다 탐색을 하지않아도 된다
+		예) 5,3,2,6
+		예) 5,3,4,5,6
+	*/
+
+	for i := 0; i < len(numbers); i++ {
+		leftNum := numbers[i]
+
+		var found bool
+		var findStartIdx = i + 1
+		var findEndIdx = len(numbers)
+
+		for j := findStartIdx; j < findEndIdx; j++ {
+			rightNum := numbers[j]
+
+			if leftNum < rightNum {
+				answer[i] = rightNum
+				found = true
+
+				findEndIdx = j //이렇게 해야 하는데 다음 반복에서 덮어 씌워짐 ㅠㅠ
+
+				break
+			}
 		}
 
-		remainDaysForDeploy = append(remainDaysForDeploy, daysNeed)
-	}
-
-	//순회하면서 뭉개버리기.::
-	var answer []int
-
-	for i, rmDays := range remainDaysForDeploy {
-		if i == 0 {
-			answer = append(answer, 1)
-			continue
-		}
-
-		//정확한 조건: 거꾸로 올라가면서 모두 다 훑어봤을 때 자기보다 더 큰 수가 진짜 하나도 없을 때만 추가를 하고,
-		//자기와 같거나 더 큰 수가 있으면은 그냥 더해주면 된다.
-		//자기보다 같거나 더 큰 수를 발견한 그 순간 탐색을 중지함으로서 속도를 빨리할 수 있겠다
-
-		if hasBiggerOrEqualNumBeforeMe(remainDaysForDeploy[:i], rmDays) {
-			answer[len(answer)-1]++
-		} else {
-			answer = append(answer, 1)
+		if !found {
+			answer[i] = -1
 		}
 	}
 
 	return answer
-}
-
-func hasBiggerOrEqualNumBeforeMe(rmDaysList []int, rmDays int) bool {
-	for i := len(rmDaysList) - 1; i >= 0; i-- {
-		pNum := rmDaysList[i]
-		if pNum >= rmDays {
-			return true
-		}
-	}
-
-	return false
 }
