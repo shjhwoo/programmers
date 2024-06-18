7e8b00b125b96f1a0667e8f0f5d4963b26892210
shjhwoo <shjhwoo@trustnhope.com>
2024-06-18 22:38:40 +0900

    뒤에 있는 큰 수 찾기

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -1,6 +1,7 @@
 package main_test
 
 import (
+	"fmt"
 	"testing"
 
 	"gotest.tools/v3/assert"
@@ -13,10 +14,10 @@ type TestCase struct {
 
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
-		{
-			numbers: []int{2, 3, 3, 5},
-			expect:  []int{3, 5, 5, -1},
-		},
+		// {
+		// 	numbers: []int{2, 3, 3, 5},
+		// 	expect:  []int{3, 5, 5, -1},
+		// },
 		{
 			numbers: []int{9, 1, 5, 3, 6, 2},
 			expect:  []int{-1, 5, 6, 6, -1, -1},
@@ -31,48 +32,70 @@ func TestSolution(t *testing.T) {
 }
 
 func solution(numbers []int) []int {
-	var answer = make([]int, len(numbers))
-	/*
-		기본로직
+	var answer = []int{}
 
-		배열 순회는 해야한다
-		자기 바로 뒤에 있는 숫자부터 비교를 시작한다 (최대한 빨리 탐색을 끝내는 것이 좋음)
+	//비교 배열을 만든다
+	compareSlice := makeCompareSlice(numbers)
 
-		뒷 큰수가 자기 바로 뒤 말고 몇칸 떨어져서 있다면,
-
-		자기 자신과 뒷 큰 수 사이에 있는 아이들은
-		자기 자신보다는 분명히 작거나 같음을 알 수 있다.
-		그리고 이 사이에 있는 아이들은 뒷 큰수보다도 작음.
-		(이 범위에서만 비교를 하면 되는 것이다)
-		그러면 배열의 끝까지 다 탐색을 하지않아도 된다
-		예) 5,3,2,6
-		예) 5,3,4,5,6
-	*/
-
-	for i := 0; i < len(numbers); i++ {
-		leftNum := numbers[i]
-
+	for i := len(numbers) - 1; i >= 0; i-- {
+		var number = numbers[i]
 		var found bool
-		var findStartIdx = i + 1
-		var findEndIdx = len(numbers)
-
-		for j := findStartIdx; j < findEndIdx; j++ {
-			rightNum := numbers[j]
-
-			if leftNum < rightNum {
-				answer[i] = rightNum
+		var foundCnt int
+		for _, compareNum := range compareSlice {
+			if number < compareNum {
 				found = true
-
-				findEndIdx = j //이렇게 해야 하는데 다음 반복에서 덮어 씌워짐 ㅠㅠ
-
-				break
+				foundCnt++
+				if foundCnt > 1 {
+					answer = answer[1:]
+				}
+				answer = append([]int{compareNum}, answer...)
 			}
 		}
 
 		if !found {
-			answer[i] = -1
+			answer = append([]int{-1}, answer...)
 		}
 	}
 
 	return answer
 }
+
+func makeCompareSlice(numbers []int) (result []int) {
+	//뒤에서부터, 최대값 모은다
+	for i := len(numbers) - 1; i >= 0; i-- {
+		if i == len(numbers)-1 {
+			result = append(result, numbers[i])
+		} else {
+			currentNum := numbers[i]
+			recentNum := result[len(result)-1]
+
+			if currentNum < recentNum {
+				result = append(result, currentNum)
+			} else if currentNum == recentNum {
+				continue
+			} else {
+				//자기보다 작은 수가 있다면 pop.
+				for {
+					if currentNum > recentNum && len(result) > 0 {
+						fmt.Println("res::", result)
+						result = result[:len(result)-1]
+
+						if len(result) == 0 {
+							recentNum = currentNum
+							result = append(result, currentNum)
+						} else {
+							recentNum = result[len(result)-1]
+						}
+					} else {
+						break
+					}
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+//https://school.programmers.co.kr/questions/43218
+//우선순위큐
