0c3135a536861a60d049a6f2e2606038f51e272f
shjhwoo <shjhwoo@trustnhope.com>
2024-07-14 11:23:39 +0900

    주차요금 계산

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -1,72 +1,103 @@
 package main_test
 
 import (
+	"sort"
+	"strconv"
+	"strings"
 	"testing"
+	"time"
 
 	"gotest.tools/v3/assert"
 )
 
 type TestCase struct {
-	numbers []int
+	fees    []int
+	records []string
 	expect  []int
 }
 
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
 		{
-			numbers: []int{2, 3, 3, 5},
-			expect:  []int{3, 5, 5, -1},
+			fees:    []int{180, 5000, 10, 600},
+			records: []string{"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"},
+			expect:  []int{14600, 34400, 5000},
 		},
 		{
-			numbers: []int{9, 1, 5, 3, 6, 2},
-			expect:  []int{-1, 5, 6, 6, -1, -1},
+			fees:    []int{120, 0, 60, 591},
+			records: []string{"16:00 3961 IN", "16:00 0202 IN", "18:00 3961 OUT", "18:00 0202 OUT", "23:58 3961 IN"},
+			expect:  []int{0, 591},
 		},
 		{
-			numbers: []int{80, 20, 4, 5, 79, 60, 10},
-			expect:  []int{-1, 79, 5, 79, -1, -1, -1},
+			fees:    []int{1, 461, 1, 10},
+			records: []string{"00:00 1234 IN"},
+			expect:  []int{14841},
 		},
 	}
 
 	for _, test := range tests {
-		ans := solution(test.numbers)
+		ans := solution(test.fees, test.records)
 		t.Log(ans, "계산값")
 		assert.DeepEqual(t, test.expect, ans)
 	}
 }
 
-func solution(numbers []int) []int {
-	var answer []int
+func solution(fees []int, records []string) []int {
+	//일단 들어온 차량번호 => 번호기준 오름차순으로 정리해야함
 
-	for i := 0; i < len(numbers); i++ {
-		answer = append(answer, -1)
+	recordMap := make(map[int][]string) //차량번호 - 입차, 출차시각 맵
+	for _, record := range records {
+		timeStamp, carNum := strings.Split(record, " ")[0], strings.Split(record, " ")[1]
+
+		carNumInt, _ := strconv.Atoi(carNum)
+
+		recordMap[carNumInt] = append(recordMap[carNumInt], timeStamp)
 	}
 
-	var stack []int
+	var carNumList []int
+	for carNum := range recordMap {
+		carNumList = append(carNumList, carNum)
+	}
 
-	for i := len(numbers) - 1; i >= 0; i-- {
+	sort.Slice(carNumList, func(i, j int) bool {
+		return carNumList[i] < carNumList[j]
+	})
 
-		for len(stack) > 0 && numbers[i] >= stack[len(stack)-1] {
-			stack = stack[:len(stack)-1]
-		}
-
-		if len(stack) > 0 && numbers[i] < stack[len(stack)-1] {
-			answer[i] = stack[len(stack)-1]
-		}
-
-		stack = append(stack, numbers[i])
+	var answer []int
+	for _, carNum := range carNumList {
+		//요금계산해서 asnwer에 넣기
+		fee := calculateFee(fees, recordMap[carNum])
+		answer = append(answer, fee)
 	}
 
 	return answer
 }
 
-//https://velog.io/@rvbear/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%92%A4%EC%97%90-%EC%9E%88%EB%8A%94-%ED%81%B0-%EC%88%98-%EC%B0%BE%EA%B8%B0Java
-/*
+func calculateFee(fees []int, inoutTime []string) int {
+	if len(inoutTime)%2 == 1 {
+		inoutTime = append(inoutTime, "23:59")
+	}
 
-이중 for문이 맞지만 배열 전체 길이를 이중으로 순회하지 않는다. 물론, 두 수 중간에, 작은 수가 너무 많다면 시간적 제한이 있을 수 있다. 분명.
-스택을 사용 => 더 큰 수가 나오면 밑에 수가 작을때 pop 시켜버린다 => 자신보다 작은 수를 없애버리는거니깐 가까운 뒤 큰 수를 빨리 찾을 수 있다.
+	var totalDuration int
+	for i := 0; i < len(inoutTime); i += 2 {
+		inTime, _ := time.Parse("15:04", inoutTime[i])
+		outTime, _ := time.Parse("15:04", inoutTime[i+1])
+		duration := outTime.Sub(inTime)
+		durationMinutes := int(duration.Minutes())
+		totalDuration += durationMinutes
+	}
 
-즉 이중포문을 도는 시간을 더 줄이는 방법임
+	baseTime, baseFee, unitTime, unitFee := fees[0], fees[1], fees[2], fees[3]
 
--1로 초기화는 => 초기값 설정으로 뒤 큰수 없는 경우 더 빨리 처리.
+	if totalDuration <= baseTime {
+		return baseFee
+	}
 
-*/
+	overTime := totalDuration - baseTime
+
+	if overTime%unitTime > 0 {
+		return baseFee + ((overTime/unitTime)+1)*unitFee
+	}
+
+	return baseFee + (overTime/unitTime)*unitFee
+}
