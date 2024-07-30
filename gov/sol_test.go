9d029177dd142a4fa6a628ad2a3b689d0f39085f
shjhwoo <shjhwoo@trustnhope.com>
2024-07-30 22:47:13 +0900

    다리를 건너는 트럭 문제

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -7,96 +7,116 @@ import (
 )
 
 type TestCase struct {
-	x      int
-	y      int
-	n      int
-	expect int
+	bridge_length int
+	weight        int
+	truck_weights []int
+	expect        int
 }
 
-/*
-문제 설명
-자연수 x를 y로 변환하려고 합니다. 사용할 수 있는 연산은 다음과 같습니다.
-
-x에 n을 더합니다
-x에 2를 곱합니다.
-x에 3을 곱합니다.
-자연수 x, y, n이 매개변수로 주어질 때, x를 y로 변환하기 위해 필요한 최소 연산 횟수를 return하도록 solution 함수를 완성해주세요. 이때 x를 y로 만들 수 없다면 -1을 return 해주세요.
-*/
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
-		// {
-		// 	x:      10,
-		// 	y:      40,
-		// 	n:      5,
-		// 	expect: 2,
-		// },
-		// {
-		// 	x:      10,
-		// 	y:      40,
-		// 	n:      30,
-		// 	expect: 1,
-		// },
-		// {
-		// 	x:      2,
-		// 	y:      5,
-		// 	n:      4,
-		// 	expect: -1,
-		// },
-		// {
-		// 	x:      2,
-		// 	y:      42,
-		// 	n:      5,
-		// 	expect: 3, //(2+5) * 3 * 2
-		// },
 		{
-			x:      2,
-			y:      21,
-			n:      5,
-			expect: 2, //(2+5) * 3 ..바로 앞의 케이스를 생각했을 때 재귀적으로 구현할 수 있다.
+			bridge_length: 2,
+			weight:        10,
+			truck_weights: []int{7, 4, 5, 6},
+			expect:        8,
 		},
+		// {
+		// 	bridge_length: 100,
+		// 	weight:        100,
+		// 	truck_weights: []int{10},
+		// 	expect:        101,
+		// },
+		// {
+		// 	bridge_length: 100,
+		// 	weight:        100,
+		// 	truck_weights: []int{10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
+		// 	expect:        110,
+		// },
 	}
 
 	for _, test := range tests {
-		ans := solution(test.x, test.y, test.n)
+		ans := solution(test.bridge_length, test.weight, test.truck_weights)
 		t.Log(ans, "계산값")
 		assert.DeepEqual(t, test.expect, ans)
 	}
 }
 
-func solution(x int, y int, n int) int {
+/*
+일차선 다리를 정해진 순으로 건너기
 
-	if (y-x)%n == 0 {
-		//x에 n만 여러 번 더해서 구할 수 있는 경우인듯.
-		//덧셈 횟수를 1씩 줄이고 곱셈 수를 늘려나가며 원하는 연산횟수를 찾는다.
-		var calcTime int 
-		maxAddTimes := (y - x) / n
-		calcTime = maxAddTimes //일단은 덧셈 횟수로 초기화
+다리에는 트럭이 최대 bridge_length대 올라갈 수 있으며
 
-		for i := maxAddTimes -1; i >= 0; i-- {
-			if (y - n * i) == x {
-				cTime := 
+다리는 weight 이하까지의 무게를 견딜 수 있습니다.
+
+모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지
+
+경과 시간	다리를 지난 트럭	다리를 건너는 트럭	대기 트럭
+0	[]	[_ _]	[7,4,5,6]
+1	[]	[_ 7]	[4,5,6]
+2   []  [7 _]   [4,5,6]
+3	[7]	[_ 4]	[5,6]
+4	[7]	[4,5]	[6]
+5	[7,4] [5 _]	[6]
+6	[7,4,5]	[_ 6]	[]
+7	[7,4,5]	[6 _]	[]
+8	[7,4,5,6]	[]	[]
+*/
+func solution(bridge_length int, maxWeight int, waiting_trucks []int) int {
+	bridge := []int{} // 왼쪽이 입구 -------> 오른쪽이 출구
+	bridge = append(bridge, waiting_trucks[0])
+	current_weight := waiting_trucks[0]
+
+	var answer = 1
+	var truckIdx = 1
+	for current_weight > 0 {
+
+		//마지막 처리를 어떻게 해야하지..ㅠㅠ
+		if truckIdx == len(waiting_trucks) {
+			if len(bridge) == 1 {
+				answer += bridge_length
+			}
+			break
+		}
+
+		/*
+			일단 새로운 트럭을 다리에 밀어 넣었을 때
+			1) 여유가 있어서 들어와도 되는 경우
+			2) 무게는 감당할 수 있지만 다리 길이가 모자라서 못 들어가는 경우
+			3) 무게가 한도를 넘어가는 경우
+		*/
+
+		newTruck := waiting_trucks[truckIdx]
+		newWeight := current_weight + newTruck
+
+		if len(bridge) < bridge_length {
+			if newWeight <= maxWeight { //1) 여유가 있어서 들어와도 되는 경우
+				bridge = append(bridge, newTruck)
+				current_weight += newTruck
+				truckIdx++
+			} else if newWeight > maxWeight {
+				//대기해야 한다. 가짜 트럭으로 0을 넣어버리기~
+				bridge = append(bridge, 0)
+			}
+		} else if len(bridge) == bridge_length {
+			if current_weight-bridge[0]+newTruck <= maxWeight {
+				firstTruck := bridge[0]
+				bridge = bridge[1:]
+				bridge = append(bridge, newTruck)
+				truckIdx++
+				current_weight = current_weight - firstTruck + newTruck
+			} else if current_weight-bridge[0]+newTruck > maxWeight {
+				//넣을 수 없다. 대신 공백으로 0을 채운다
+				firstTruck := bridge[0]
+				bridge = bridge[1:]
+				bridge = append(bridge, 0)
+				current_weight -= firstTruck
 			}
 		}
+
+		answer++
+
 	}
 
 	return answer
-}
-
-func calcMulTimeBy2or3orAddN(num, x, n int) int {
-	var calcTime int
-	for num%2 == 0 {
-		num = num / 2
-		calcTime++
-	}
-
-	for num%3 == 0 {
-		num = num / 3
-		calcTime++
-	}
-
-	if num == x+n {
-		calcTime++
-	}
-
-	return calcTime
 }
