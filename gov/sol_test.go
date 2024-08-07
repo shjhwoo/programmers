2aedf637d859614cdb352f1c0b23a1b06e2c7a18
shjhwoo <shjhwoo@trustnhope.com>
2024-08-07 23:02:33 +0900

    안된다 안돼

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -14,15 +14,20 @@ type TestCase struct {
 
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
+		// {
+		// 	priorities: []int{2, 1, 3, 2},
+		// 	location:   2,
+		// 	expect:     1,
+		// },
+		// {
+		// 	priorities: []int{1, 1, 9, 1, 1, 1},
+		// 	location:   0,
+		// 	expect:     5,
+		// },
 		{
-			priorities: []int{2, 1, 3, 2},
-			location:   2,
-			expect:     1,
-		},
-		{
-			priorities: []int{1, 1, 9, 1, 1, 1},
-			location:   0,
-			expect:     5,
+			priorities: []int{1, 2, 3, 4, 5, 6}, //  5 1 2 3 4
+			location:   4,
+			expect:     2,
 		},
 	}
 
@@ -44,7 +49,6 @@ func solution(priorities []int, location int) int {
 	var cursor int
 
 	for nonZeroCnt > 0 && cursor < len(priorities) {
-
 		if isCusrorHighest(priorities) {
 			priorities[cursor] = 0
 			nonZeroCnt--
@@ -52,18 +56,19 @@ func solution(priorities []int, location int) int {
 
 			priorities = append(priorities[1:], priorities[0])
 
-			locationMap[cursor+1] = locationMap[cursor]
-			locationMap[cursor] = len(priorities)
+			locationMap[cursor+1] = 1             //맨 앞으로 가기
+			locationMap[cursor] = len(priorities) //맨 뒤로 가기
 
-			for c := 0; c < len(locationMap); c++ {
-				if c > cursor+1 {
-					locationMap[c] = locationMap[c-1] + 1
-				}
+			var boundary = len(priorities)
 
-				if c < cursor {
-					locationMap[c] = locationMap[c+1] - 1
-				}
+			//순서 떙기기..
+			var i int = 2
+			for {
+				locationMap[cursor+i] = locationMap[cursor+i-1] + 1 //어디까지??
+
+				i++
 			}
+
 		}
 
 		cursor++
