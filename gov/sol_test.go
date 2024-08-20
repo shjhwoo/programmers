bc9d06f204b83cecd8b06e34fe778653c0ea935e
shjhwoo <shjhwoo@trustnhope.com>
2024-08-20 22:47:19 +0900

    트라이 실습

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -1,7 +1,6 @@
 package main_test
 
 import (
-	"container/heap"
 	"fmt"
 	"testing"
 
@@ -9,8 +8,7 @@ import (
 )
 
 type TestCase struct {
-	no     int
-	works  []int
+	words  []string
 	expect int
 }
 
@@ -18,98 +16,80 @@ type TestCase struct {
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
 		{
-			no:     4,
-			works:  []int{4, 3, 3},
-			expect: 12,
+			words:  []string{"go", "gone", "guild"},
+			expect: 7,
 		},
-		{
-			no:     2,
-			works:  []int{3, 3, 3},
-			expect: 17,
-		},
+		// {
+		// 	words:  []string{"abc", "def", "ghi", "jklm"},
+		// 	expect: 4,
+		// },
+		// {
+		// 	words:  []string{"word", "war", "warrior", "world"},
+		// 	expect: 15,
+		// },
 	}
 
 	for _, test := range tests {
-		ans := solution(test.no, test.works)
+		ans := solution(test.words)
 		t.Log(ans, "계산값")
 		assert.DeepEqual(t, test.expect, ans)
 	}
 }
 
-// 남은 일의 작업량을 숫자로 매기고 배상비용을 최소화
-// 배상 비용은 각 선박의 완성까지 남은 일의 작업량을 제곱하여 모두 더한 값이 됩니다.
-func solution(no int, works []int) int {
-	//일단 배열의 모든 인수가 다 최소화가 되어야 한다.
+type TrieNode struct {
+	CurrentChar string
+	PointerMap  map[string]*TrieNode
+}
 
-	//가장 간단히 하려면 빼는 경우의 수 모두 다 찾고, 각각 계산해서 최소값을 구함.
-	//그러면 일단은 어찌 되었든 간에 works 배열을 최대 힙으로 정렬하고,
-	// 가장 상위의 원소에서 무조건 1을 뺀 다음에, 그 다음으로 최대값이 되는 원소를 찾아
-	// 루트로 올리는 정렬을 한다.
-	// 이런 과정을 반복하면 최소값을 구할 수 있다.
-	// 최대가 되는 숫자들을 어찌되었든 간에 최소로 하려는 것이다.
+func (tn *TrieNode) Add(word string) {
+	found, node := tn.Find(word)
+	if !found {
 
-	worksHeap := &IntHeap{}
-	for _, work := range works {
-		worksHeap.Push(work)
+		if node.PointerMap == nil {
+			node.PointerMap = make(map[string]*TrieNode)
+		}
+		//이때만 추가.
+		node.PointerMap[word[:len(word)-1]] = &TrieNode{
+			CurrentChar: word[len(word)-1:],
+		}
+	}
+}
+
+// 분기점을 찾아주는..(찾은 경우 찾은 노드, 못 찾은 경우 부모 노드라도 주기?)
+func (tn *TrieNode) Find(word string) (bool, *TrieNode) {
+	var found bool
+	var foundNode *TrieNode
+
+	if tn.CurrentChar == word {
+		found = true
+		foundNode = tn
+		return found, foundNode
+	} else {
+		foundNode = tn //못 찾은 경우 부모.
 	}
 
-	heap.Init(worksHeap)
+	subword := word[:len(word)-1]
 
-	for no > 0 {
-		longestWork := worksHeap.Pop().(int)
-		longestWork--
-
-		//다시 힙에 넣어준다.. 정렬은 알아서 함.
-		worksHeap.Push(longestWork)
-
-		//뺀다.
-		no--
+	for pointer, nextNode := range tn.PointerMap {
+		if subword == nextNode.CurrentChar {
+			//해당 노드로 내려가서 계속 찾아줌..
+			newSub := subword + pointer
+			found, foundNode = nextNode.Find(newSub)
+			break
+		}
 	}
 
-	fmt.Println(*worksHeap)
+	return found, foundNode
+}
 
-	var answer int
-	for worksHeap.Len() > 0 {
-		elem := worksHeap.Pop().(int)
-
-		answer += elem * elem
+func solution(words []string) int {
+	//트라이 자료구조를 만들어야 한다.
+	trie := TrieNode{}
+	for _, word := range words {
+		trie.Add(word)
 	}
 
-	return answer
-}
+	fmt.Println(trie, "trie 구조 만든거")
 
-type IntHeap []int
-
-func (h IntHeap) Len() int {
-	return len(h)
-}
-
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] > h[j]
-}
-
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
-}
-
-func (h *IntHeap) Push(elem any) {
-	*h = append(*h, elem.(int))
-}
-
-func (h *IntHeap) Pop() any {
-	copy := *h //슬라이스는 배열의 주소값이기 때문에 메서드 정의 시 포인터로 정의하지 않으면 원본이 훼손됨
-	result := copy[0]
-	*h = copy[1:]
-
-	return result
-}
-
-func TestHeap(t *testing.T) {
-	h := &IntHeap{2, 1, 7}
-
-	heap.Init(h)
-	fmt.Println(*h)
-
-	heap.Push(h, 4)
-	assert.DeepEqual(t, IntHeap{1, 2, 7, 4}, *h)
+	return 0
 }
