4f9c0efbfba35a57e6de8f5cc6cf0dde2aeb48ae
shjhwoo <shjhwoo@trustnhope.com>
2024-08-27 23:02:11 +0900

    단어 검색

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -1,7 +1,9 @@
 package main_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"strings"
 	"testing"
 
 	"gotest.tools/v3/assert"
@@ -15,18 +17,18 @@ type TestCase struct {
 // 포인트는 인덱스를 같이 저장하는 것..!!
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
-		{
-			words:  []string{"go", "gone", "guild"},
-			expect: 7,
-		},
+		// {
+		// 	words:  []string{"go", "gone", "guild"},
+		// 	expect: 7,
+		// },
 		// {
 		// 	words:  []string{"abc", "def", "ghi", "jklm"},
 		// 	expect: 4,
 		// },
-		// {
-		// 	words:  []string{"word", "war", "warrior", "world"},
-		// 	expect: 15,
-		// },
+		{
+			words:  []string{"word", "war", "warrior", "world"},
+			expect: 15,
+		},
 	}
 
 	for _, test := range tests {
@@ -36,60 +38,102 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-type TrieNode struct {
-	CurrentChar string
-	PointerMap  map[string]*TrieNode
-}
-
-func (tn *TrieNode) Add(word string) {
-	found, node := tn.Find(word)
-	if !found {
-
-		if node.PointerMap == nil {
-			node.PointerMap = make(map[string]*TrieNode)
-		}
-		//이때만 추가.
-		node.PointerMap[word[:len(word)-1]] = &TrieNode{
-			CurrentChar: word[len(word)-1:],
-		}
-	}
-}
-
-// 분기점을 찾아주는..(찾은 경우 찾은 노드, 못 찾은 경우 부모 노드라도 주기?)
-func (tn *TrieNode) Find(word string) (bool, *TrieNode) {
-	var found bool
-	var foundNode *TrieNode
-
-	if tn.CurrentChar == word {
-		found = true
-		foundNode = tn
-		return found, foundNode
-	} else {
-		foundNode = tn //못 찾은 경우 부모.
-	}
-
-	subword := word[:len(word)-1]
-
-	for pointer, nextNode := range tn.PointerMap {
-		if subword == nextNode.CurrentChar {
-			//해당 노드로 내려가서 계속 찾아줌..
-			newSub := subword + pointer
-			found, foundNode = nextNode.Find(newSub)
-			break
-		}
-	}
-
-	return found, foundNode
-}
-
 func solution(words []string) int {
 	//트라이 자료구조를 만들어야 한다.
+
 	trie := TrieNode{}
 	for _, word := range words {
-		trie.Add(word)
+		trie.Insert(word)
 	}
 
-	fmt.Println(trie, "trie 구조 만든거")
+	b, _ := json.MarshalIndent(trie, "", "	")
 
-	return 0
+	fmt.Println(string(b), "만든 trie 확인하기")
+
+	var wordMap = make(map[string]bool)
+	for _, word := range words {
+		wordMap[word] = true
+	}
+
+	var answer int
+	for _, word := range words {
+
+		fmt.Println("개수::", trie.Count(word, wordMap))
+
+		answer += trie.Count(word, wordMap)
+	}
+
+	return answer
+}
+
+type TrieNode struct {
+	Value   string
+	NextMap map[string]*TrieNode
+}
+
+func (tn *TrieNode) Insert(word string) {
+	currentNode := tn
+	charList := strings.Split(word, "")
+
+	for _, char := range charList {
+
+		if currentNode.NextMap == nil {
+			currentNode.NextMap = make(map[string]*TrieNode)
+		}
+
+		if _, ok := currentNode.NextMap[char]; !ok {
+			//이때만 넣어준다..
+			currentNode.NextMap[char] = &TrieNode{
+				Value:   currentNode.Value + char,
+				NextMap: make(map[string]*TrieNode),
+			}
+		}
+
+		currentNode = currentNode.NextMap[char]
+	}
+}
+
+func (tn *TrieNode) Has(word string) bool {
+	currentNode := tn
+	charList := strings.Split(word, "")
+
+	for _, char := range charList {
+		if _, ok := currentNode.NextMap[char]; !ok {
+			return false
+		}
+
+		currentNode = currentNode.NextMap[char]
+	}
+
+	return true
+}
+
+func (tn *TrieNode) Count(word string, wordMap map[string]bool) int {
+	currentNode := tn
+	charList := strings.Split(word, "")
+
+	var cnt int
+
+	for _, char := range charList {
+		if _, ok := currentNode.NextMap[char]; !ok {
+			break
+		}
+
+		cnt++
+		currentNode = currentNode.NextMap[char]
+		if len(currentNode.NextMap) == 1 {
+			if wordMap[currentNode.Value] && currentNode.Value != word {
+				cnt++
+				break
+			}
+
+			if wordMap[currentNode.Value] {
+				break
+			}
+
+			//카운트 문제..!! -- 마지막 케이스.
+		}
+	}
+
+	return cnt
 }
--- solution.js
+++ solution.js
@@ -1,4 +1,49 @@
-function solution(words) {
-  var answer = 0;
-  return answer;
+//trie
+
+class Node {
+  constructor(value = "") {
+    this.value = value;
+    this.children = new Map();
+  }
 }
+
+class Trie {
+  constructor() {
+    this.root = new Node();
+  }
+
+  insert(string) {
+    let currentNode = this.root;
+
+    for (const char of string) {
+      if (!currentNode.children.has(char)) {
+        currentNode.children.set(char, new Node(currentNode.value + char));
+      }
+
+      currentNode = currentNode.children.get(char);
+    }
+  }
+
+  has(string) {
+    let currentNode = this.root;
+
+    for (const char of string) {
+      if (!currentNode.children.has(char)) {
+        return false;
+      }
+
+      currentNode = currentNode.children.get(char);
+    }
+
+    return true;
+  }
+}
+
+const tree = new Trie();
+
+tree.insert("cat");
+tree.insert("can");
+
+console.log(tree.has("ca"));
+console.log(tree.has("can"));
+console.log(tree.has("cap"));
