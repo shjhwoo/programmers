d3174660dbcfb5f719245f8a8e12e5db2c428c46
shjhwoo <shjhwoo@trustnhope.com>
2024-09-07 09:35:54 +0900

    삽입 정렬 구현

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -87,3 +87,43 @@ func getHighestRemainderIdx(rmNumbers []int) int {
 
 	return resIdx
 }
+
+func TestInsertSort(t *testing.T) {
+	var tests = []TestCase{
+		{
+			numbers: []int{7, 4, 5, 1, 3},
+			expect:  []int{1, 3, 4, 5, 7},
+		},
+	}
+
+	for _, test := range tests {
+		ans := InsertSort(test.numbers)
+		t.Log(ans, "계산값")
+		assert.DeepEqual(t, test.expect, ans)
+	}
+}
+
+func InsertSort(numbers []int) []int {
+	for i := 1; i < len(numbers); i++ { //배열의 요소가 5개라면, 4번만큼 반복할 수 있다.
+		/*
+			i == 1일때는 0과 비교
+			i == 2일때는 1 => 1  vs 0
+			...
+			3 vs 2 , 2 vs 1 , 1 vs 0
+			4 vs 3 , 3 vs 2 , 2 vs 1 , 1 vs 0
+
+		*/
+
+		for j := i; j >= 1; j-- {
+			left := numbers[j-1]
+			right := numbers[j]
+
+			if left > right {
+				numbers[j] = left
+				numbers[j-1] = right
+			}
+		}
+	}
+
+	return numbers
+}
