91f34ed677e6ca2a962729403d4b64e2fa06a394
shjhwoo <shjhwoo@trustnhope.com>
2024-09-21 12:15:49 +0900

    91%통과

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -1,7 +1,6 @@
 package main_test
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 
@@ -16,16 +15,16 @@ type TestCase struct {
 
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
-		// {
-		// 	number: "1924",
-		// 	k:      2,
-		// 	expect: "94",
-		// },
-		// {
-		// 	number: "1231234",
-		// 	k:      3,
-		// 	expect: "3234",
-		// },
+		{
+			number: "1924",
+			k:      2,
+			expect: "94",
+		},
+		{
+			number: "1231234",
+			k:      3,
+			expect: "3234",
+		},
 		{
 			number: "4177252841",
 			k:      4,
@@ -41,43 +40,48 @@ func TestSolution(t *testing.T) {
 
 // 최대한 앞에 있는 것 => 그 다음으로 제일 작은 수부터 빼낸다.
 func solution(number string, k int) string { //결과값에는 뺴야 하는 숫자의 인덱스 위치를 저장해둔다
-	var afterBiggerNumMap = make(map[int]bool)
 
-	// var nums []int
-	// for idx, numCh := range strings.Split(number, "") {
-	// 	n, _ := strconv.Atoi(numCh)
-	// 	nums = append(nums, n)
-	// }
+	var stack []string
 
-	// sort.Ints(nums) //오름차순 정렬이다. 상대적으로
-
+	var rmCnt int
+	var lastIdx int
 	for idx, numCh := range strings.Split(number, "") {
-		for _, afterNum := range strings.Split(number, "")[idx+1:] {
-			if numCh < afterNum { //여기에다가 조건을 하나 더 넣던지 해야함. 현재 수 뒤에, 더 작은 숫자들이 존재하고 있다면 빼기에 곤란하다.
-				afterBiggerNumMap[idx] = true
+		if idx == 0 {
+			stack = append(stack, numCh)
+		} else {
+
+			top := stack[len(stack)-1]
+
+			if top >= numCh {
+				stack = append(stack, numCh)
+			} else {
+				for top < numCh {
+					stack = stack[:len(stack)-1]
+					rmCnt++
+					if rmCnt == k {
+						lastIdx = idx
+						break
+					}
+
+					if len(stack) > 0 {
+						top = stack[len(stack)-1]
+					}
+
+					if len(stack) == 0 {
+						break
+					}
+				}
+
+				stack = append(stack, numCh)
+
+			}
+
+			if rmCnt == k {
 				break
 			}
+
 		}
 	}
 
-	fmt.Println("afterBiggerNumMap: ", afterBiggerNumMap)
-
-	var answer string
-	var rmCnt int
-	for idx, numCh := range strings.Split(number, "") {
-		//현재 숫자의 인덱스 이후의 뒤에 더 큰 숫자들이 존재한다면 빼도 된다. => 그러면 현재 인덱스 --- 자기보다 더 큰 숫자들이 존재
-		//빼낸 갯수 다 채웠으면은 스탑한다.
-		//조건 부족..
-		if _, afterBiggerNumExists := afterBiggerNumMap[idx]; afterBiggerNumExists {
-			rmCnt++
-		} else {
-			answer += numCh
-		}
-
-		if rmCnt == k {
-			break
-		}
-	}
-
-	return answer
+	return strings.Join(stack, "") + number[lastIdx+1:]
 }
