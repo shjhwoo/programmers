f39943c3ada714861df89bd22703a96fc4aef646
shjhwoo <shjhwoo@trustnhope.com>
2024-09-21 11:42:01 +0900

    큰 수 만들기

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -1,32 +1,31 @@
 package main_test
 
 import (
-	"sort"
-	"strconv"
+	"fmt"
 	"strings"
 	"testing"
 
 	"gotest.tools/v3/assert"
 )
 
-type DFSGraphTestCase struct {
+type TestCase struct {
 	number string
 	k      int //제거할 수!
 	expect string
 }
 
 func TestSolution(t *testing.T) {
-	var tests = []DFSGraphTestCase{
-		{
-			number: "1924",
-			k:      2,
-			expect: "94",
-		},
-		{
-			number: "1231234",
-			k:      3,
-			expect: "3234",
-		},
+	var tests = []TestCase{
+		// {
+		// 	number: "1924",
+		// 	k:      2,
+		// 	expect: "94",
+		// },
+		// {
+		// 	number: "1231234",
+		// 	k:      3,
+		// 	expect: "3234",
+		// },
 		{
 			number: "4177252841",
 			k:      4,
@@ -40,67 +39,45 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-func solution(number string, k int) string {
+// 최대한 앞에 있는 것 => 그 다음으로 제일 작은 수부터 빼낸다.
+func solution(number string, k int) string { //결과값에는 뺴야 하는 숫자의 인덱스 위치를 저장해둔다
+	var afterBiggerNumMap = make(map[int]bool)
 
-	/*
-		제거를 하면 몇자리 숫자가 되는지를 파악해야 한다?
-		음 일단은 앞쪽에 있는 숫자가 최대한 크도록 제거해나가야한다.
+	// var nums []int
+	// for idx, numCh := range strings.Split(number, "") {
+	// 	n, _ := strconv.Atoi(numCh)
+	// 	nums = append(nums, n)
+	// }
 
-		제일 작은 숫자들을 빼내야 한다.
+	// sort.Ints(nums) //오름차순 정렬이다. 상대적으로
 
-		제일 작은 숫자 같은게 여러 개 있으면 그 중 제일 앞에 있는 숫자를 제거한다.
-
-		그러면 일단은 숫자 메타정보 맵을 만들어보자.
-
-		해당 숫자: 해당 숫자가 어느어느 인덱스에 있는지를 알려주기. map[int][]int
-
-		조심해야하는경우... 예) 10009170 에서 숫자 3개만 빼라고 했을때 앞의 숫자 0을 빼면 19170, 뒤의 숫자 0을 빼면 10917
-	*/
-
-	idxToRmMap := buildNumberMeta(number, k)
-
-	var answer string
-	var splitted = strings.Split(number, "")
-	for idx, numCh := range splitted {
-		//제거조건에 안 맞으면은 answer에 가져다붙인다
-		if _, needRm := idxToRmMap[idx]; !needRm {
-			answer = answer + numCh
-		}
-	}
-
-	return answer
-}
-
-func buildNumberMeta(number string, k int) map[int]bool {
-
-	var foundNums []int
-
-	var resMap = make(map[int][]int)
 	for idx, numCh := range strings.Split(number, "") {
-		n, _ := strconv.Atoi(numCh)
-
-		foundNums = append(foundNums, n)
-
-		resMap[n] = append(resMap[n], idx) //인덱스 순서대로 붙을거야..
-	}
-
-	sort.Ints(foundNums)
-
-	//위 결과값을 인덱스 순서대로 정렬
-	var res = make(map[int]bool)
-	for _, nkey := range foundNums {
-
-		for _, i := range resMap[nkey] {
-			res[i] = true
-			if len(res) == k {
+		for _, afterNum := range strings.Split(number, "")[idx+1:] {
+			if numCh < afterNum { //여기에다가 조건을 하나 더 넣던지 해야함. 현재 수 뒤에, 더 작은 숫자들이 존재하고 있다면 빼기에 곤란하다.
+				afterBiggerNumMap[idx] = true
 				break
 			}
 		}
+	}
 
-		if len(res) == k {
+	fmt.Println("afterBiggerNumMap: ", afterBiggerNumMap)
+
+	var answer string
+	var rmCnt int
+	for idx, numCh := range strings.Split(number, "") {
+		//현재 숫자의 인덱스 이후의 뒤에 더 큰 숫자들이 존재한다면 빼도 된다. => 그러면 현재 인덱스 --- 자기보다 더 큰 숫자들이 존재
+		//빼낸 갯수 다 채웠으면은 스탑한다.
+		//조건 부족..
+		if _, afterBiggerNumExists := afterBiggerNumMap[idx]; afterBiggerNumExists {
+			rmCnt++
+		} else {
+			answer += numCh
+		}
+
+		if rmCnt == k {
 			break
 		}
 	}
 
-	return res
+	return answer
 }
