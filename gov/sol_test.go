56ea940b1ccc595fa4a4025f87339031dda5420e
shjhwoo <shjhwoo@trustnhope.com>
2024-10-08 21:33:01 +0900

    크루스칼 알고리즘 사용한 섬 연결하기 풀이

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -3,10 +3,34 @@ package main_test
 import (
 	"sort"
 	"testing"
+
+	"gotest.tools/v3/assert"
 )
 
-func TestSolution(t *testing.T) {
+type Case struct {
+	n      int
+	costs  [][]int
+	expect int
+}
 
+func TestSolution(t *testing.T) {
+	var tests = []Case{
+		{
+			n: 4,
+			costs: [][]int{
+				{0, 1, 1},
+				{0, 2, 2},
+				{1, 2, 5}, //사이클 발생
+				{1, 3, 1},
+				{2, 3, 8},
+			},
+			expect: 4,
+		},
+	}
+
+	for _, test := range tests {
+		assert.DeepEqual(t, test.expect, solution(test.n, test.costs))
+	}
 }
 
 type Node struct {
@@ -15,37 +39,52 @@ type Node struct {
 }
 
 func solution(n int, costs [][]int) int {
-	/*
-		1. 일단 제일 작은 간선 값을 가지는 경우가 앞에 오도록 정렬한다.
 
-		2. 1에서 정렬한 배열의 0번째 인덱스 요소부터 탐색을한다 => 서로소 집합을 만든다.
-
-	*/
-
+	///간선을 가중치 기준 오름차순 정렬. 그래야 최소를 계산가능
 	sort.Slice(costs, func(i, j int) bool {
-		return costs[i][2] > costs[j][2]
+		return costs[i][2] < costs[j][2]
 	})
 
-	var nodes []Node
-	for _, cost := range costs {
-		nodes = append(nodes, Node{
-			Connection: cost,
-		})
-	}
-
-	//일단은 자기자신으로 초기화
-	parentMap := make(map[int]int)
+	//부모 정보를 저장할 슬라이스선언.
+	var parentInfo []int
 	for i := 0; i < n; i++ {
-		parentMap[i] = i
+		parentInfo = append(parentInfo, i)
 	}
 
-	for _, node := range nodes {
-		start := node.Connection[0]
-		dest := node.Connection[1]
-
-		//부모 찾기(재귀적으로-- 자기자신이 부모면 탈출)
-
+	var answer int
+	for _, cost := range costs {
+		if !compare(parentInfo, cost[0], cost[1]) { //두 원소의 부모가 같으면 사이클
+			answer += cost[2] //같지 않은 경우에만 하나의 집합으로 만들수있다 (결국 하나의 부모로 귀결된다는 의미)
+			union(parentInfo, cost[0], cost[1])
+		}
 	}
 
-	return 0
+	return answer
+}
+
+func compare(parentInfo []int, start int, dest int) bool {
+	sp := findParent(start, parentInfo)
+	dp := findParent(dest, parentInfo)
+
+	return sp == dp
+}
+
+func union(parentInfo []int, start int, dest int) {
+	start = findParent(start, parentInfo)
+	dest = findParent(dest, parentInfo)
+
+	if start < dest {
+		parentInfo[dest] = start
+	} else {
+		parentInfo[start] = dest
+	}
+}
+
+func findParent(node int, parentInfo []int) int {
+	if parentInfo[node] == node {
+		return node
+	}
+
+	parentInfo[node] = findParent(parentInfo[node], parentInfo)
+	return parentInfo[node]
 }
