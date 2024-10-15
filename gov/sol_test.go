b0b75a200aad4700dfe08bfbcde6ac5e3b6c4aeb
shjhwoo <shjhwoo@trustnhope.com>
2024-10-15 21:52:42 +0900

    보석쇼핑 - 투포인터 풀이 완료

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -3,8 +3,6 @@ package main_test
 import (
 	"testing"
 
-	"sort"
-
 	"gotest.tools/v3/assert"
 )
 
@@ -43,169 +41,91 @@ func TestSolution(t *testing.T) {
 //가장 짧은 구간을 찾아서 구매
 
 func solution(gems []string) []int {
-	gems = append([]string{""}, gems...) //인덱스 헷갈리지 마라공..
 
 	gemMap := make(map[string]bool)
 	for _, gem := range gems {
-		if gem == "" {
-			continue
-		}
 		gemMap[gem] = true
 	}
 
+	gems = append([]string{""}, gems...)
+
+	//처음 콜렉션을 만들어 둔다..
+	collected := make(map[string]int)
+
 	startIdx := 1
-	endIdx := startIdx + len(gemMap) - 1
+	endIdx := 1
 
-	var candidates [][]int
+	collected[gems[startIdx]] = 1
 
-	for startIdx <= endIdx && endIdx < len(gems) {
-		shoppingList := gems[startIdx : endIdx+1]
+	var answer = []int{1, len(gems) - 1}
 
-		if shopListHasAllKind(shoppingList, gemMap) {
-			candidates = append(candidates, []int{startIdx, endIdx})
+	for startIdx < len(gems) && endIdx < len(gems) {
+		if len(gemMap) == len(collected) {
+
+			//더 줄일 수 있는지..
+			if endIdx-startIdx < answer[1]-answer[0] {
+				answer = []int{startIdx, endIdx}
+			}
+
+			collected[gems[startIdx]]--
+
+			if collected[gems[startIdx]] == 0 {
+				delete(collected, gems[startIdx])
+			}
+
 			startIdx++
-			endIdx = startIdx + len(gemMap) - 1
 		} else {
-			//인덱스를 이동(endIdx만)
 			endIdx++
+			if endIdx == len(gems) {
+				break
+			}
+			collected[gems[endIdx]]++
 		}
 	}
 
-	// for i, candidate := range candidates {
-	// 	fmt.Println(i, "번째 쇼핑리스트 확인: ", gems[candidate[0]:candidate[1]+1], "인덱스: ", candidate)
-	// }
-
-	sort.Slice(candidates, func(i, j int) bool {
-
-		iStIdx := candidates[i][0]
-		iEndIdx := candidates[i][1]
-
-		jStIdx := candidates[j][0]
-		jEndIdx := candidates[j][1]
-
-		if len(gems[iStIdx:iEndIdx+1]) == len(gems[jStIdx:jEndIdx+1]) {
-			return iStIdx < jStIdx
-		}
-		return len(gems[iStIdx:iEndIdx+1]) < len(gems[jStIdx:jEndIdx+1])
-	})
-
-	//fmt.Println(candidates)
-
-	return candidates[0]
-}
-
-func shopListHasAllKind(shoppingList []string, gemMap map[string]bool) bool {
-	shopMap := make(map[string]bool)
-
-	for _, shopGem := range shoppingList {
-		shopMap[shopGem] = true
-	}
-
-	for gem := range gemMap {
-		if !shopMap[gem] {
-			return false
-		}
-	}
-
-	return true
+	return answer
 }
 
 /*
 javascript:
 
+포인트:
+- 부분합을 구하여 저장해두는 맵을 loop 밖에 둬야 한다 => 맵끼리 빠른 비교 유도 (길이만으로 알 수 있으니까.)
+- 경계값에 유의하여야 한다.
+
 function solution(gems) {
-    gems = ["", ...gems]
 
+    const gemKind = new Set(gems);
 
-    const gemMap = new Map()
-    gems.forEach(gem => {
-        if (gem === ""){
-            return
+    const collection = new Map();
+
+    let start = 0
+    let end = 0
+
+    collection.set(gems[start], 1)
+
+    var answer = [1, gems.length];
+
+    while (start < gems.length && end < gems.length){
+        if (gemKind.size === collection.size) {
+
+            if (end - start < answer[1] - answer[0]){
+                answer = [start+1, end+1]
+            }
+
+            collection.set(gems[start], collection.get(gems[start]) - 1)
+
+            if ( collection.get(gems[start]) === 0) {
+                collection.delete(gems[start])
+            }
+
+            start++
+        } else {
+            end++
+            collection.set(gems[end], (collection.get(gems[end]) || 0) + 1)
         }
-
-        gemMap.set(gem, true)
-    })
-
-    const gemMapArr = Array.from(gemMap , ([key, value]) => key)
-
-    let start = 1
-    let end = start + gemMap.size - 1
-
-    const shopMap = new Map()
-    gems.slice(start, end + 1).forEach(gem => {
-        if (!shopMap.get(gem)) {
-            shopMap.set(gem,1)
-        }else{
-            shopMap.set(gem, shopMap.get(gem) + 1)
-        }
-    })
-
-
-    let hasAll = shopListHasAllKind(shopMap, gemMapArr)
-    if (hasAll) {
-        candidates.push([start,end])
-        start++
-        end = start + gemMap.size - 1
-
-        //맵을 조정한다
-        const gemToDel = gems[start]
-        if (shopMap.get(gemToDel) && shopMap.get(gemToDel) > 0) {
-            shopMap.set(gemToDel, shopMap.get(gemToDel) - 1)
-        }
-
-        if (shopMap.get(gemToDel) && shopMap.get(gemToDel) == 0) {
-            shopMap.delete(gemToDel)
-        }
-    }else{
-        end++
     }
 
-    const newGem = gems[end]
-    if (!shopMap.get(newGem)) {
-        shopMap.set(newGem, 1)
-    }else{
-        shopMap.set(newGem, shopMap.get(newGem) + 1)
-    }
-
-    const candidates = []
-    while (start <= end && end < gems.length) {
-
-        console.log("start: ",start,"end: ",end,"shopMap: ", shopMap)
-
-    }
-
-    candidates.sort(sortCandidates);
-
-    return candidates[0]
+    return answer;
 }
-
-//이 체크하는 로직을 바꿔야한다. 새로 추가한 보석이 여기에 들어간는지 안들어간는지 보면됨..
-function shopListHasAllKind(shopMap, gemMapArr) {
-    for (const gem of gemMapArr){
-        if (!shopMap.get(gem)){
-            return false
-        }
-    }
-
-    return true
-}
-
-function sortCandidates(can1, can2) {
-    const iStartIdx = can1[0];
-    const iEndIdx = can1[1];
-
-    const jStartIdx = can2[0];
-    const jEndIdx = can2[1];
-
-    const iGemListLength = iEndIdx - iStartIdx;
-    const jGemListLength = jEndIdx - jStartIdx;
-
-    // First, sort by the length of the range (smaller range comes first)
-    if (iGemListLength !== jGemListLength) {
-        return iGemListLength - jGemListLength;
-    }
-
-    // If the ranges are of equal length, sort by the starting index
-    return iStartIdx - jStartIdx;
-})
 */
