9fe3a8557d9fd49a0975c0873032aa9bbd2ca118
shjhwoo <shjhwoo@trustnhope.com>
2024-10-16 22:55:38 +0900

    N-Queen , 백트래킹 문제

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -1,131 +1,120 @@
 package main_test
 
 import (
+	"fmt"
+	"sort"
+	"strings"
 	"testing"
 
 	"gotest.tools/v3/assert"
 )
 
 type TestCase struct {
-	gems   []string
-	expect []int
+	n      int
+	expect int
 }
 
 func TestSolution(t *testing.T) {
 	var tests = []TestCase{
 		{
-			gems:   []string{"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"},
-			expect: []int{3, 7},
-		},
-		{
-			gems:   []string{"AA", "AB", "AC", "AA", "AC"},
-			expect: []int{1, 3},
-		},
-		{
-			gems:   []string{"XYZ", "XYZ", "XYZ"},
-			expect: []int{1, 1},
-		},
-		{
-			gems:   []string{"ZZZ", "YYY", "NNNN", "YYY", "BBB"},
-			expect: []int{1, 5},
+			n:      4,
+			expect: 2,
 		},
 	}
 
 	for _, test := range tests {
-		assert.DeepEqual(t, test.expect, solution(test.gems))
+		assert.DeepEqual(t, test.expect, solution(test.n))
 	}
 }
 
-//진열된 모든 종류의 보석을
-//적어도 1개 이상 포함하는
-//가장 짧은 구간을 찾아서 구매
+func solution(n int) int {
 
-func solution(gems []string) []int {
+	//처음 점 0,0부터 시작해야 한다
+	//특정한 조건을 만족하는 것만 탐색하고 나머지는 탐색하지 않도록 조건문 작성
+	//즉 절대로 답이 될 수 없는 것은 탐색을 종료한다.
 
-	gemMap := make(map[string]bool)
-	for _, gem := range gems {
-		gemMap[gem] = true
-	}
+	var candidates = make(map[string]bool)
 
-	gems = append([]string{""}, gems...)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			startPos := []int{i, j}
 
-	//처음 콜렉션을 만들어 둔다..
-	collected := make(map[string]int)
+			qPosList := getQPosList(startPos, n)
 
-	startIdx := 1
-	endIdx := 1
+			if len(qPosList) == n {
+				sort.Slice(qPosList, func(i, j int) bool {
+					return qPosList[i][0] < qPosList[j][0]
+				})
 
-	collected[gems[startIdx]] = 1
+				posKey := makePosKey(qPosList)
 
-	var answer = []int{1, len(gems) - 1}
-
-	for startIdx < len(gems) && endIdx < len(gems) {
-		if len(gemMap) == len(collected) {
-
-			//더 줄일 수 있는지..
-			if endIdx-startIdx < answer[1]-answer[0] {
-				answer = []int{startIdx, endIdx}
+				if _, exist := candidates[posKey]; !exist {
+					candidates[posKey] = true
+				}
 			}
 
-			collected[gems[startIdx]]--
-
-			if collected[gems[startIdx]] == 0 {
-				delete(collected, gems[startIdx])
-			}
-
-			startIdx++
-		} else {
-			endIdx++
-			if endIdx == len(gems) {
-				break
-			}
-			collected[gems[endIdx]]++
 		}
 	}
 
-	return answer
+	return len(candidates)
 }
 
-/*
-javascript:
+func getChessBoard(n int) [][]bool {
+	var board [][]bool
 
-포인트:
-- 부분합을 구하여 저장해두는 맵을 loop 밖에 둬야 한다 => 맵끼리 빠른 비교 유도 (길이만으로 알 수 있으니까.)
-- 경계값에 유의하여야 한다.
+	for i := 0; i < n; i++ {
+		line := []bool{}
+		for j := 0; j < n; j++ {
+			line = append(line, true)
+		}
 
-function solution(gems) {
+		board = append(board, line)
+	}
 
-    const gemKind = new Set(gems);
-
-    const collection = new Map();
-
-    let start = 0
-    let end = 0
-
-    collection.set(gems[start], 1)
-
-    var answer = [1, gems.length];
-
-    while (start < gems.length && end < gems.length){
-        if (gemKind.size === collection.size) {
-
-            if (end - start < answer[1] - answer[0]){
-                answer = [start+1, end+1]
-            }
-
-            collection.set(gems[start], collection.get(gems[start]) - 1)
-
-            if ( collection.get(gems[start]) === 0) {
-                collection.delete(gems[start])
-            }
-
-            start++
-        } else {
-            end++
-            collection.set(gems[end], (collection.get(gems[end]) || 0) + 1)
-        }
-    }
-
-    return answer;
+	return board
 }
-*/
+
+func getQPosList(startPos []int, n int) [][]int {
+	//계속 반복하면서 탐색!!!
+	chessBoard := getChessBoard(n)
+
+	var result = [][]int{startPos}
+
+	queue := [][]int{startPos}
+
+	for len(queue) > 0 {
+		startRow := queue[0][0]
+		startCol := queue[0][1]
+		queue = queue[1:]
+
+		//빼낸 지점에서부터 탐색 가능한 다음의 지점들을 넣는다.
+
+		for r, row := range chessBoard {
+			for c, col := range row {
+				if r == startRow || c == startCol { // 피해야 하는 조건을 명시한다
+					chessBoard[r][c] = false
+					continue
+				}
+
+				//대각선 지점인 경우도 피하기!!
+				if 같은대각선라인이면 {
+					chessBoard[r][c] = false
+					continue
+				}
+
+			}
+		}
+	}
+
+	return result
+}
+
+func makePosKey(qPosList [][]int) string {
+	var result []string
+
+	for _, qpos := range qPosList {
+		result = append(result, fmt.Sprintf("(%d,%d)", qpos[0], qpos[1]))
+	}
+
+	return strings.Join(result, "-")
+}
