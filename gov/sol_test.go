4010a73f12a6d1b74011f05921d94dba0b86cbc0
shjhwoo <shjhwoo@trustnhope.com>
2024-11-11 22:01:19 +0900

    단어퍼즐 풀이과정 이해중

--- gov/sol_test.go
+++ gov/sol_test.go
@@ -1,8 +1,6 @@
 package main_test
 
 import (
-	"sort"
-	"strings"
 	"testing"
 
 	"gotest.tools/v3/assert"
@@ -39,65 +37,35 @@ func TestSolution(t *testing.T) {
 }
 
 func solution(strs []string, t string) int {
+	var preMap = make(map[string]bool)
 
-	/*
-		사용 가능한 단어 조각들을 담고 있는 배열 strs와
-		 완성해야 하는 문자열 t가 매개변수로 주어질 때,
-		 주어진 문장을 완성하기 위해 사용해야 하는 단어조각 개수의
-		 최솟값을 return
-
-		 만약 주어진 문장을 완성하는 것이 불가능하면 -1을 return
-	*/
-
-	/*
-
-		ㅁㅁㅁ ...                 .... ㅁㅁ
-		ㄴ 단어를 구성하는 글자들의 뭉침 덩어리들
-
-		최소라고 했기 때문에..
-
-		일단 0번째 인덱스에 있는 부분문자열이 t의 prefix인지 검사. , 그러면 남은 뒷 부분에 대해서도 같은 방식으로 알아서 검사가능
-		만약에 prefix가 아니라면 다음 인덱스로 넘어가서 검사한다.
-		prefix가 나올때까지 반복하고 찾았으면 answer에 카운팅을 해둔다. 그리고 t에서 prefix를 잘라내고 남은 부분에 대해 같은 방법으로 반복한다.
-		단, prefix를 찾더라도, 최대한 길이가 긴 prefix를 찾아야 한다.
-		=> 그런데 이렇게 한 것 때문에 뒤에 남은 부분에 맞은 prefix를 못 찾았다면, 카운팅을 취소한다.
-	*/
-	var prefixCandidates []string
 	for _, pre := range strs {
-		if strings.HasPrefix(t, pre) {
-			prefixCandidates = append(prefixCandidates, pre)
+		preMap[pre] = true
+	}
+
+	var memoMap []int
+
+	for j := 0; j < len(t)+1; j++ {
+		memoMap = append(memoMap, 0)
+	}
+
+	//제일 작은 문제부터 정의한다.
+	for i := 1; i < len(t); i++ {
+		//제일 작은 문제는 i == 1일때.
+		subWord := t[0:i] //단어 ba 
+
+		for j := i-1; j >= 0; j-- {}
+		s2 := t[0:i-j] //이전의 작은 문제 b 
+		if 
+
+		if preMap[subWord] {
+			//기억해둔다.
+			memoMap[i] = 1 //?
+		} else {
+			//없는 경우. -1
+			memoMap[i] = -1 //?
 		}
 	}
 
-	if len(prefixCandidates) == 0 {
-		return -1
-	}
-
-	//후보군들을 찾았다
-
-	sort.Slice(prefixCandidates, func(i, j int) bool {
-		return len(prefixCandidates[i]) > len(prefixCandidates[j])
-	})
-
-	var answer = 0
-
-	for _, pre := range prefixCandidates {
-		newT := strings.TrimPrefix(t, pre)
-
-		if newT == "" {
-			return 1
-		}
-
-		//그 외에는.. 한번 더 호출이 가능
-		cnt := solution(strs, newT)
-
-		if cnt == -1 {
-			continue
-		}
-
-		answer += cnt + 1
-		break
-	}
-
-	return answer
+	return 0
 }
